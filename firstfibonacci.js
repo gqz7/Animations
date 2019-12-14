0f1d03423432cbef3f50b33fdb74f5d90efa925c
GabrielQSherman <gabrielquinn.photography@gmail.com>
2019-12-14 14:52:41 -0500

    FINALLY i got a static fibonacci spiral image. there is also code to create boxes that is current has one context.stroke line commented out so just the spiral appears it took some trial and error but ultimatley i had to use a model image and try to understand where my origin was at all times to create the boxes in the corret patter.  once i knew the boxes were in their correct palce i could start creating an arc from one corner to another. it took some trial and error to find where my origin point for the arc needed to be. then creating an arc with a turn radius of 1/2 Pi Now i can move on to some fun ways to animate this creation. it was recursion that was neccessary to complete this. but implementing it took much time.

--- firstfibonacci.js
+++ firstfibonacci.js
@@ -1,34 +1,3 @@
-window.onload = function () {
-     var canvas = document.getElementById("canvas"),
-        context = canvas.getContext("2d"),
-        width = canvas.width = this.innerWidth, //this refers to window
-        height = canvas.height = this.innerHeight,
-        time = 0;
-
-
-    context.strokeStyle = "white";
-
-    let fibOrigin = 100;
-    let fibLength = width -200;
-    context.beginPath();
-        context.rect(fibOrigin, fibOrigin, fibLength, fibLength-1100);
-        context.stroke();
-    let prevOrigin = fibOrigin;
-    let prevLength = fibLength * 1.681;
-
-    let currentOrigin = fibOrigin,
-        currentLength = fibLength;
-
-
-    for (let i = 0; i < 1; i++) {
-
-        
-        context.beginPath();
-            context.rect(currentOrigin,currentOrigin,currentLength,currentLength);
-            context.stroke();
-
-    }
-
 
 window.onload = function () {
      var canvas = document.getElementById("canvas"),
@@ -39,28 +8,40 @@ window.onload = function () {
 
     context.strokeStyle = "white";
 
+    context.translate(width/2,height/2); //to start at center
+    // context.translate(width,0); //start at diffrent point
 
-    let fibOrigin = 100;
-    let fibLength = width -200;
-    context.beginPath();
-        context.rect(fibOrigin, fibOrigin, fibLength, fibLength-1100);
-        context.stroke();
-    let prevOrigin = fibOrigin;
-    let prevLength = fibLength * 1.681;
+    let start = 1;
 
-    let currentOrigin = fibOrigin,
-        currentLength = fibLength;
+    createFib(start, start);
 
+    function createFib(curFib, prevFib) {
 
-    for (let i = 0; i < 1; i++) {
+        console.log(`prevL: ${prevFib}, curFib: ${curFib}`);
 
-        
+        fibLength = prevFib + curFib;
+
         context.beginPath();
-            context.rect(currentOrigin,currentOrigin,currentLength,currentLength);
-            context.stroke();
+        context.rect(0,0,fibLength,fibLength);
+        // context.stroke()
 
+        context.beginPath();
+        context.arc(0,fibLength, fibLength, -.5 *Math.PI, 0);
+        context.stroke();
+
+        context.translate(fibLength,0);
+        context.rotate(.5 * Math.PI);
+        context.translate(fibLength,0);
         
+        prevFib = curFib;
+        curFib = fibLength;
         
-        
+        if (prevFib < 1000) {
+            createFib(curFib, prevFib);
+        }
+
         
     }
+
+
+}
