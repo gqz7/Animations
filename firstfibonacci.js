f8e512ea0d371a18a535435e3c00f4ed13cdd177
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-12-14 15:21:22 -0500

    Add files via upload

--- firstfibonacci.js
+++ firstfibonacci.js
@@ -8,35 +8,42 @@ window.onload = function () {
 
     context.strokeStyle = "white";
 
+    context.translate(width/2,height/2); //to start at center
+    // context.translate(width,0); //start at diffrent point
 
-    let boxOrigin = width/20;
-    let boxWidth = width - width/10;
-    let boxHeight = height - height/5;
-    context.beginPath();
-    context.rect(boxOrigin, boxOrigin, boxWidth, boxHeight);
-    context.stroke();
+    let fibLength = 2, previousfibLength = 2;
 
+    let start = 2;
 
+    createFib(start, start);
 
-    // let prevOrigin = fibOrigin;
-    // let prevLength = fibLength * 1.681;
+    function createFib(curFib, prevFib) {
 
-    
+        console.log(`FibL: ${fibLength}, prevL: ${prevFib}, curFib: ${curFib}`);
 
+        fibLength = prevFib + curFib;
 
-    // for (let i = 0; i < 1; i++) {
+        context.beginPath();
+        context.rect(0,0,fibLength,fibLength);
+        context.stroke()
 
-    //     let currentOrigin = fibOrigin,
-    //     currentLength = fibLength;
+        context.beginPath();
+        context.arc(fibLength,0, fibLength, .5 * Math.PI, Math.PI);
+        // context.stroke();
+
+        context.translate(fibLength,0);
+        context.rotate(-.5 * Math.PI);
+        context.translate(0,fibLength);
+        
+        prevFib = curFib;
+        curFib = fibLength;
+        
+        if (prevFib < 1000) {
+            createFib(curFib, prevFib);
+        }
 
         
-    //         context.beginPath();
-    //         context.rect(currentOrigin,currentOrigin,currentLength,currentLength);
-    //         context.arc(fibOrigin, fibOrigin, 50, 0, 2 * Math.PI);
-    //         context.stroke();
+    }
 
 
-        
-        
-    // }
 }
