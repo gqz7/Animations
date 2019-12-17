3872d98c9cd0cf10fa775b194dcac9b0b3fd559d
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-12-17 00:38:53 -0500

    Add files via upload

--- fibonacci_diagram.js
+++ fibonacci_diagram.js
@@ -4,50 +4,58 @@ window.onload = function () {
         context = canvas.getContext("2d"),
         width = canvas.width = this.innerWidth, //this refers to window
         height = canvas.height = this.innerHeight,
-        time = 0;
+        time = 0, startLength, arr = [1];
+
+        
+    let prvNum = 1, curNum = 1;
+
+    create_fib_arr()
+    function create_fib_arr() {
+        
+        let fibNum = prvNum + curNum;
+
+        if (prvNum < height) {
+            
+            prvNum = curNum;
+            curNum = fibNum;
+
+            arr.push(fibNum);
+
+            create_fib_arr()
+        } else {
+            startLength = curNum;
+        }
+
+        
+        
+    }
 
     context.strokeStyle = "white";
 
     // context.translate(width/2,height/2); //to start at center
-    // context.translate(width,0); //start at diffrent point
+    context.translate(0,height); //start at diffrent point
 
-    let start = 2;
-
-    context.translate(0,0); 
     context.rotate(-.5 * Math.PI);
-    // createFib(start, start);
 
-    // context.translate(100,100); 
+    function create_fib_dia() {
 
-    createFib(height, height);
-
-    function createFib(curFib, prevFib) {
-
-        console.log(`prevL: ${prevFib}, curFib: ${curFib}`);
-
-        fibLength = prevFib - curFib;
-
+        for (let i = arr.length - 3; i > -1; i--) {
         context.beginPath();
-        context.rect(0,0,fibLength,fibLength);
+        context.rect(0,0,arr[i],arr[i]);
         context.stroke()
 
         context.beginPath();
-        context.arc(0,fibLength, fibLength, -.5 *Math.PI, 0);
+        context.arc(0,arr[i], arr[i], -.5 *Math.PI, 0);
         context.stroke();
 
-        context.translate(fibLength,0);
+        context.translate(arr[i],0);
         context.rotate(.5 * Math.PI);
-        context.translate(fibLength,0);
+        context.translate(arr[i],0);
         
-        prevFib = curFib;
-        curFib = fibLength;
-        
-        if (prevFib > 10) {
-            createFib(curFib, prevFib);
         }
-
         
     }
 
+    
 
 }
