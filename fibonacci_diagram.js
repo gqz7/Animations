1537cbc44c1284b91925e77225ed5199d5792f1e
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-12-17 00:00:29 -0500

    Add files via upload

---
+++ fibonacci_diagram.js
@@ -0,0 +1,53 @@
+
+window.onload = function () {
+     var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        width = canvas.width = this.innerWidth, //this refers to window
+        height = canvas.height = this.innerHeight,
+        time = 0;
+
+    context.strokeStyle = "white";
+
+    // context.translate(width/2,height/2); //to start at center
+    // context.translate(width,0); //start at diffrent point
+
+    let start = 2;
+
+    context.translate(0,0); 
+    context.rotate(-.5 * Math.PI);
+    // createFib(start, start);
+
+    // context.translate(100,100); 
+
+    createFib(height, height);
+
+    function createFib(curFib, prevFib) {
+
+        console.log(`prevL: ${prevFib}, curFib: ${curFib}`);
+
+        fibLength = prevFib - curFib;
+
+        context.beginPath();
+        context.rect(0,0,fibLength,fibLength);
+        context.stroke()
+
+        context.beginPath();
+        context.arc(0,fibLength, fibLength, -.5 *Math.PI, 0);
+        context.stroke();
+
+        context.translate(fibLength,0);
+        context.rotate(.5 * Math.PI);
+        context.translate(fibLength,0);
+        
+        prevFib = curFib;
+        curFib = fibLength;
+        
+        if (prevFib > 10) {
+            createFib(curFib, prevFib);
+        }
+
+        
+    }
+
+
+}
