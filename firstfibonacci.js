2c11b02a9c70d51496b6defecd13191f340c684e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-04 23:17:35 -0500

    updating repo

--- firstfibonacci.js
+++ firstfibonacci.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-
-window.onload = function () {
-     var canvas = document.getElementById("canvas"),
-        context = canvas.getContext("2d"),
-        width = canvas.width = this.innerWidth, //this refers to window
-        height = canvas.height = this.innerHeight,
-        time = 0;
-
-    context.strokeStyle = "white";
-
-    context.translate(width/2,height/2); //to start at center
-    // context.translate(width,0); //start at diffrent point
-
-    let start = 1;
-
-    createFib(start, start);
-
-    function createFib(curFib, prevFib) {
-
-        console.log(`prevL: ${prevFib}, curFib: ${curFib}`);
-
-        fibLength = prevFib + curFib;
-
-        context.beginPath();
-        context.rect(0,0,fibLength,fibLength);
-        // context.stroke()
-
-        context.beginPath();
-        context.arc(0,fibLength, fibLength, -.5 *Math.PI, 0);
-        context.stroke();
-
-        context.translate(fibLength,0);
-        context.rotate(.5 * Math.PI);
-        context.translate(fibLength,0);
-        
-        prevFib = curFib;
-        curFib = fibLength;
-        
-        if (prevFib < 1000) {
-            createFib(curFib, prevFib);
-        }
-
-        
-    }
-
-
-=======
 
 window.onload = function () {
      var canvas = document.getElementById("canvas"),
@@ -94,5 +46,4 @@ window.onload = function () {
     }
 
 
->>>>>>> 2813b41503342a1f3abae9e63ab71c9bc4f780b0
 }
