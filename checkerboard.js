3bf8e101bb32fb87d3c6b031148d7c97695377d5
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-07 23:36:37 -0500

    a line animates across the screen

--- checkerboard.js
+++ checkerboard.js
@@ -6,11 +6,29 @@ window.onload = function() {
 		width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
 
+        let subW = width, subH = height;
 
-        context.beginPath()
-        context.moveTo(0,0);
-        context.lineTo(width, height);
-        context.stroke()
+        animate()
+        function animate() {
+
+            subH -= 5;
+            subW -= 10;
+
+            context.beginPath()
+            context.moveTo(0,0);
+            context.lineTo(width - subW, height - subH);
+
+            context.stroke()
+
+            setTimeout(window.requestAnimationFrame, 0, (animate))
+        }
+
+
+        // context.beginPath()
+        // context.moveTo(0,0);
+        // context.lineTo(width, height);
+
+        // context.stroke()
 
 
 }
