58641dea12bbb881f55a9ec982c5013fa4174173
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-07 23:22:59 -0500

    establish working html and js

--- checkerboard.js
+++ checkerboard.js
@@ -1,1 +1,16 @@
 //background color must be white
+
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight;
+
+
+        context.beginPath()
+        context.moveTo(0,0);
+        context.lineTo(width, height);
+        context.stroke()
+
+
+}
