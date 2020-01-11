e4b8ebabd9b1eedcf0653df5de1440d08f31016a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-11 15:05:54 -0500

    as the grid translates it leaves a trail of color

---
+++ checkerboard_rainbow_columns.js
@@ -0,0 +1,147 @@
+//background color must be white
+
+window.onload = function() {
+
+    //INITIAL VARIABLE DECLERATIONS
+
+	let canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        time = 0;
+        
+		width = canvas.width = window.innerWidth,       //width of the canvas
+        height = canvas.height = window.innerHeight,   //height of the canvas
+        size = 20,                                    //determins size of each square
+
+        //logic is included to make sure columnLimit is an odd number, this will make sure each row starts will the opposite of how to previous row started
+        columnLimit =  (Math.ceil(width/size) % 2 == 0) ? Math.ceil(width/size) + 1: Math.ceil(width/size),     //how many columns in the grid
+
+        rowLimit = ((height/size) + 2),    //how many rows in the grid
+        columnCycles = 0,                 //handles if too many columns of squares have been made on a row
+        rowsCycles = 0,                  //handles if too many rows of squares have been made on the grid
+        startWithWhite = true,          //switches from true to flase for each row to make sure the pattern is alike to a checker/chess board
+
+
+        originx = 0, originy = 0,
+
+        p1 = {x: originx, y: originy}, p2 = {x: size + originx, y: originy}, p3 = {x: size + originx, y: size + originy}, p4 = {x: originx, y: size + originy},
+        squares = [];
+
+        // console.log(columnLimit, (width/size));
+        
+
+        // console.log(lim);
+
+        //ANIMATION CYCLE
+
+        
+        // columnLimit -= 50;
+        // rowLimit -= 20;
+
+        // context.save() //saves the context at 0,0 (upper left corner) to go back to for each new line
+        
+        // context.translate(500,200);
+
+
+        // console.log(squares);
+        
+        animate()
+        function animate() {
+            time++
+    
+            // create_grid()
+            // console.log('test');
+            
+            // return
+            
+            // clear()
+            context.fillStyle = 'hsl(' + (time*3) + ', 100%, 60%)';
+
+            
+            create_grid()
+
+             
+            context.translate(20,0)
+            
+            
+
+            // return
+            setTimeout(window.requestAnimationFrame, 0, (animate));
+        }
+
+        // FUNCTIONS
+
+        function clear() { 
+            context.save();
+            context.setTransform(1, 0, 0, 1, 0, 0);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.restore();
+        }
+
+
+        function create_square(p1,p2,p3,p4) {
+
+            let square = {
+                p1, p2, p3, p4
+            };
+
+            // squares.push(square);
+
+            context.beginPath();
+            context.moveTo(p1.x, p1.y);
+            context.lineTo(p2.x, p2.y);
+            context.lineTo(p3.x, p3.y);
+            context.lineTo(p4.x, p4.y);
+            context.lineTo(p1.x,p1.y);
+
+            if (startWithWhite) {
+                context.fill();
+                startWithWhite = false;
+            } else {
+                startWithWhite = true;
+            }
+            
+
+        }
+
+        function create_row(){
+            
+
+            while (columnCycles < columnLimit) {
+                create_square(p1,p2,p3,p4);
+                context.translate(size, originy);
+                columnCycles++
+            }
+
+            columnCycles = 0
+            
+        }
+
+        function create_grid() {
+
+            context.save()
+
+            while (rowsCycles < rowLimit) {
+                context.save()
+                create_row()
+                context.restore()
+                context.translate(originx,size);
+                rowsCycles++
+            }
+            rowsCycles = 0;
+
+
+            context.restore()
+
+        }
+
+       
+
+
+}
+//a test to see if the context is in the right postion
+// context.strokeStyle = 'white';
+//             context.beginPath()
+//             context.moveTo(0,0);
+//             context.lineTo(width,height);
+            
+//             context.stroke()
