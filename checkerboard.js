3f49b7e78076d1ebdc52d5ef55a1f58d7f947677
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-14 14:12:49 -0500

    final commit for checkerboard i will explore more ways to create this pattern in a way that allows me to know the position of all squares at one time. this will be the last of this 3-stage-function processes that creates this checkerboard layout

--- checkerboard.js
+++ checkerboard.js
@@ -10,7 +10,7 @@ window.onload = function() {
         
 		width = canvas.width = window.innerWidth,       //width of the canvas
         height = canvas.height = window.innerHeight,   //height of the canvas
-        size = 20,                                    //determins size of each square
+        size = 100,                                    //determins size of each square
 
         //logic is included to make sure columnLimit is an odd number, this will make sure each row starts will the opposite of how to previous row started
         columnLimit =  (Math.ceil(width/size) % 2 == 0) ? Math.ceil(width/size) + 1: Math.ceil(width/size),     //how many columns in the grid
@@ -19,7 +19,7 @@ window.onload = function() {
         columnCycles = 0,                 //handles if too many columns of squares have been made on a row
         rowsCycles = 0,                  //handles if too many rows of squares have been made on the grid
         startWithWhite = true,          //switches from true to flase for each row to make sure the pattern is alike to a checker/chess board
-        originx = 0, originy = 0,      //if these vars' values are modified the grid starting position will also be modified
+        originx = 333, originy = 333,  //if these vars' values are modified the grid starting position will also be modified 
 
         p1 = {x: originx, y: originy}, p2 = {x: size + originx, y: originy}, p3 = {x: size + originx, y: size + originy}, p4 = {x: originx, y: size + originy},
         squares = [];
@@ -27,43 +27,40 @@ window.onload = function() {
         //Animation Modifiers 
 
         // ---Sets the size of the grid, else the whole screen wil be used
-        columnLimit += 30;
-        rowLimit -= 70;
+        columnLimit += 42;
+        rowLimit += 70;
 
         // ---Translates the top left corner of the grid,
-        // else it will start at the top left corner of the window
+        // any orign point that is set above 0 will allow a window into the other quadrents
+        originx = originx;
+        originy = originy;
+        //  the area of your window into the second quadrent is (originx * originy) as long as they are both positive values
+        //example: 333 * 333 = 110,889 pixeles available in the second quadrent
+        //         width * 333 = pixeles available in third quadrent
+        //          height * 333 = pixeles avaiable in the first quadrent
+
+        //The height and width are currently set to 'inner.Width/Height(respectively)' 
         // context.translate(500,200);
 
-        //Color, else fill style must be set in the animation cycle
-        context.fillStyle = 'white';
-
-        // console.log(squares);
         //ANIMATION CYCLE
         animate()
         function animate() {
             time++
-    
-            // create_grid()
-            // console.log('test');
-            
-            // return
-            
-            clear()
+
+            // clear() only uncomment if you want to see the current state the created grid, on each frame. 
+            //each frame will stay painted onto the canvas until a clear function is called
             
             create_grid()
 
-            // originx++
-            columnLimit-=2;
-            rowLimit++;
-            // context.translate(1,1)
-
-            // return
-            setTimeout(window.requestAnimationFrame, 30, (animate));
+            // return if you uncomment this only the first grid will be made
+            size-=.314;
+            setTimeout(window.requestAnimationFrame, 30, (animate)); //number of milliseconds wiat period between each frame
         }
 
         // FUNCTIONS
 
         function clear() { 
+
             context.save();
             context.setTransform(1, 0, 0, 1, 0, 0);
             context.clearRect(0, 0, canvas.width, canvas.height);
@@ -71,38 +68,42 @@ window.onload = function() {
         }
 
 
-        function create_square(p1,p2,p3,p4) {
-
-            let square = {
-                p1, p2, p3, p4
-            };
-
-            // squares.push(square);
-
+        //draw square onto canvas then fill
+        function create_square(p1,p2,p3,p4) { //this function creates the square line by line. and will then fill the shape with either white or black
+        
             context.beginPath();
+
             context.moveTo(p1.x, p1.y);
             context.lineTo(p2.x, p2.y);
             context.lineTo(p3.x, p3.y);
             context.lineTo(p4.x, p4.y);
             context.lineTo(p1.x,p1.y);
 
+            //this function uses a boolean switch between white and black every time
+
+            //YOU CAN CHANGE THE COLORS BY PUTTING A COLOR NAME INTO THE SINGLE QUOTES :)
+            // ie. 'Aquamarine', 'LightGoldenRodYellow', 'Fuchsia'
             if (startWithWhite) {
+                context.fillStyle = 'white'; // find all the colors @ w3schools.com/colors/colors_names.asp 
                 context.fill();
                 startWithWhite = false;
+
             } else {
+                context.fillStyle = 'black';
+                context.fill();
                 startWithWhite = true;
             }
             
-
         }
 
         function create_row(){
-            
 
-            while (columnCycles < columnLimit) {
+            while (columnCycles < columnLimit) { //it will contiue to make new square every 
+
                 create_square(p1,p2,p3,p4);
                 context.translate(size, 0);
                 columnCycles++
+
             }
 
             columnCycles = 0
@@ -122,21 +123,11 @@ window.onload = function() {
                 context.translate(0,size);
                 rowsCycles++
             }
-            rowsCycles = 0;
 
+            rowsCycles = 0;
 
             context.restore()
 
         }
 
-       
-
-
 }
-//a test to see if the context is in the right postion
-// context.strokeStyle = 'white';
-//             context.beginPath()
-//             context.moveTo(0,0);
-//             context.lineTo(width,height);
-            
-//             context.stroke()
