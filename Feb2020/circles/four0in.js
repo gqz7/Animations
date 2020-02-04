f5e0dc74ebef086dcb0a3ae4aa858035d0ed5f07
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-04 15:30:31 -0500

    new animation just using circles, using ctx.arc method

--- Feb2020/circles/four0in.js
+++ Feb2020/circles/four0in.js
@@ -7,37 +7,54 @@ let canvas = document.getElementById("canvas"),
     time = 0;
     
 const width = canvas.width = window.innerWidth,       //width of the canvas
-      height = canvas.height = window.innerHeight,   //height of the canvas
-      size = 48.1;                                  //determins size of each square
+      height = canvas.height = window.innerHeight;   //height of the canvas
+
 
    
     //ANIMATION CYCLE
 
     context.translate(width/2, height/2)
+
+    context.save()
     
     animate()
     function animate() {
 
         
 
-        time++
+        if (time > 800) {
+            time = 0;
+            clear()
 
-        context.rotate(Math.PI/2);
-
-        if (time % 50 == 0) clear()
-
-
-        for (let i = -10; i < 10; i++) {
-
-            context.translate(time/10,i*4);
-
-            let curClr = time *10;
-            
-            make_circle(time/10, time/100, curClr, time);
-            
         }
 
-        setTimeout(window.requestAnimationFrame, 6, (animate));
+        let dividend = 4;
+        //Math.floor(time / 100);
+
+        time++
+
+        context.rotate(Math.PI/2); 
+
+        context.save()
+        for (let i = -10; i < 10; i+=.25) {
+
+            context.translate(time/-i*2,-i*7);
+
+            let curClr = time * 13,
+                    x = time/100,
+                    y = time/100,
+                size = time/2;
+            
+            make_circle(x, y, curClr, size);
+            
+        }
+        context.restore()
+
+        if (time % 200 == 0) {
+            // clear()
+        }
+
+        setTimeout(window.requestAnimationFrame, 0, (animate));
     }
 
     // FUNCTIONS
@@ -47,7 +64,7 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
         context.beginPath()
         context.arc(x, y,  size, 0, 2 * Math.PI)
 
-        context.strokeStyle = 'hsl('+color+', 80%, 30%)'
+        context.strokeStyle = 'hsl('+color+', 100%, 70%)'
         context.stroke()
 
         
