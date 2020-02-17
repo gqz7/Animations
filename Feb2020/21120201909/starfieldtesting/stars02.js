654f0c6a287a4210f74b4e9e65f93f452aa3bf51
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-17 10:19:31 -0500

    endless journey through a globular cluster

--- Feb2020/21120201909/starfieldtesting/stars02.js
+++ Feb2020/21120201909/starfieldtesting/stars02.js
@@ -8,7 +8,11 @@ let canvas = document.getElementById("canvas"),
     
 const width = canvas.width = window.innerWidth,       //width of the canvas
       height = canvas.height = window.innerHeight,   //height of the canvas
-      delay = 10000;                                //determins durration of time(ms) between each frame
+      delay = 10;                                    //determins durration of time(ms) between each frame
+
+let  speed = 7;                                   //sets the speed at which stars travel away from the center
+
+      context.translate(width/2, height/2) //setting the origin (0,0) to the center of the screen makes it easier to calculate where stars will spawn (will change this later so the origin can be set with a var) 
 
 
       let Stars = []; //this array will store the values of the current stars on the screen
@@ -24,15 +28,29 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
         clear() //clears the screen
 
-        
+        renderStars() //displays each star from its position in the Stars array 
+
+        moveStars(speed) //moves the position of each start slightly
 
         setTimeout( () => {
+
+            //  console.log(time);
+
             
-            if (time < 1000) {
+            if (time < 100) {
 
-                 // console.log(time);
-
+                
                  animate()
+                
+            } else if (time < 10000) {
+
+                if (speed < 21) {
+
+                    speed += .001
+                    
+                }
+
+                animate()
                 
             } else {
 
@@ -45,15 +63,14 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
     // FUNCTIONS
 
-    function make_circle(x, y, hue, size){
+    function make_circle(x, y, hue, size, lightness){
 
         context.beginPath()
         context.arc(x, y,  size, 0, 2 * Math.PI)
 
-        let saturation = 100,
-            lightnes = 77;
+        let saturation = 70;
 
-        context.strokeStyle = `hsl(${hue}, ${saturation}%, ${lightnes}%)`;
+        context.strokeStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
         context.stroke()
 
         
@@ -62,15 +79,16 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
     function create_star_field() {
 
-        for (let i = 0; i < 700; i++) {
+        for (let i = 0; i < 400; i++) {
 
             let hue = Math.random() * 360,
-                    x = Math.random() * width,
-                    y = Math.random() * height,
-                    size = 2;
+                    x = (Math.random() * width) - width /2,
+                    y = (Math.random() * height) - height /2,
+                    size = 1,
+                    lightness = -10;
              
             Stars.push({
-                x: x, y: y, hue: hue, radius: size
+                x: x, y: y, hue: hue, radius: size, lightness: lightness
             });
             
         }
@@ -91,8 +109,54 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
         for (let i = 0; i < Stars.length; i++) {
             
-            make_circle(Star[i].x, Star[i].y, Star[i].hue, Star[i].radius);
+            make_circle(Stars[i].x, Stars[i].y, Stars[i].hue, Stars[i].radius, Stars[i].lightness);
             
         }
 
+    }
+
+    function moveStars(speed) {
+
+        for (let i = 0; i < Stars.length; i++) {
+
+            let NewX = Stars[i].x * (1 + speed/1000),
+                NewY = Stars[i].y * (1 + speed/1000);
+
+
+                if (NewX > width/2 || NewX < -width/2 || NewY > height/2 || NewY < -height/2) {
+
+                    Stars.splice(i, 1); //if it goes off screen, delete it from the stars to be rendered
+
+                    addStar() // then add a new one to replace it
+
+                    i--
+
+                } else {
+
+                    Stars[i].x = NewX;
+                    Stars[i].y = NewY;
+
+                    Stars[i].lightness +=.88
+                    
+                    Stars[i].radius += 3.14/1000
+
+                }
+           
+        }
+        
+    }
+
+
+    function addStar() { //when one star dies another is born
+
+        let hue = Math.random() * 360,
+        x = (Math.random() * width/4) - width /8,
+        y = (Math.random() * height/2) - height /4,
+        size = 1,
+        lightness = 0;
+ 
+        Stars.push({
+            x: x, y: y, hue: hue, radius: size, lightness: lightness
+        });
+        
     }
