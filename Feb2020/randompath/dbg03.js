2256792490a577b1fbf4c710a12c917c279e74d7
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-22 17:21:47 -0500

    update animation

--- Feb2020/randompath/dbg03.js
+++ Feb2020/randompath/dbg03.js
@@ -1,3 +1,4 @@
+
 //INITIAL VARIABLE DECLERATIONS
 
 const slider = document.getElementById('slider');
@@ -16,14 +17,16 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
     let Objects = [];
 
-    create_objects()
+    create_objects(100)
     animate()
 
     function animate() {
 
         time++
 
-        clear()
+        // clear()
+
+        context.rotate(time/200000)
 
         render_objects()
         
@@ -31,15 +34,11 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
         // console.log(Objects.length);
         
 
-        if (time % 100 == 0) {
-            create_objects()
-        }
-
         change_properties()
 
         setTimeout( () => {
             
-            if (time < 3000) {
+            if (time < 5000) {
 
                  animate()
                 
@@ -61,9 +60,9 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
         let saturation = 100;
 
-        context.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+        context.strokeStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 
-        context.fill()
+        context.stroke()
         
     }
 
@@ -74,18 +73,16 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
         let saturation = 100;
 
-        context.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+        context.strokeStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 
-        context.fill()
+        context.stroke()
         
     }
 
     function render_triangle(x, y, hue, size, lightness){
 
     
-        context.fillStyle = `hsl(${hue}, 100%, ${lightness}%)`;
-
-        context.save()
+        context.strokeStyle = `hsl(${hue}, 100%, ${lightness}%)`;
 
         context.translate(x,y)
 
@@ -101,35 +98,31 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
             
             context.lineTo(size,size);
 
-        context.fill()
-
-        context.restore()
-        
+        context.stroke()
+ 
     }
 
 
 
-    function create_objects() {
+    function create_objects(num_of_objs) {
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < num_of_objs; i++) {
 
-            hue = Math.random() * 360,
+            hue =( Math.random() * 130) + time/4 + 100,
 
             lightness = 0,
 
-            x = Math.random() * width/8,
-            y = Math.random() * height/8,
-            size = Math.random() * 100,
+            x = Math.random() * width/32 + 70,
+            y = Math.random() * width/32 + 70,
+            size = (Math.random() * 30),
 
-            growthRandomizer = Math.random() > .5 ?  false : true,
-
-            ranAngle = Math.round((Math.random() * (Math.PI * 2)) * 10) / 10;
             
-
-            // console.log(ranAngle);
+            ranAngle = Math.round((Math.random() * (Math.PI * 2)) * 100) / 100,
             
-
-            Objects.unshift({x: x, y: y, hue: hue, size: size, lightness: lightness, growing: growthRandomizer, fading: false, angle: ranAngle})
+            shapes = ['circle', 'square', 'triangle']
+            shape = shapes[Math.round(Math.random()*shapes.length)];
+   
+            Objects.unshift({x: x, y: y, hue: hue, size: size, lightness: lightness, growing: true, fading: false, angle: ranAngle, shape: shape})
             
         }
         
@@ -147,17 +140,30 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
             context.rotate(angle)
 
-            Objects[i].x += 1;
+            Objects[i].x += (Objects[i].x / 100)
                
-            Objects[i].y += 1
+            Objects[i].y += (Objects[i].y / 100)
 
 
             if (Objects[i].x > width/2 || Objects[i].x < -width/2 || Objects[i].y > width/2 || Objects[i].y <  -width/2)  {
                 Objects.splice(i, 1)
                 i--
-            } 
 
-            render_triangle(Objects[i].x, Objects[i].y, Objects[i].hue, Objects[i].size, Objects[i].lightness);
+                create_objects(1)
+            } else if (Objects[i].shape === 'triangle') {
+
+                render_triangle(Objects[i].x, Objects[i].y, Objects[i].hue, Objects[i].size, Objects[i].lightness);
+            
+            } else if (Objects[i].shape === 'square') {
+                
+                render_square(Objects[i].x, Objects[i].y, Objects[i].hue, Objects[i].size, Objects[i].lightness);
+            
+            } else if (Objects[i].shape === 'circle') {
+                
+                render_circle(Objects[i].x, Objects[i].y, Objects[i].hue, Objects[i].size, Objects[i].lightness);
+            
+            }
+
             
 
             context.restore()
@@ -170,23 +176,23 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
         for (let i = 0; i < Objects.length; i++) {
     
-               if (Objects[i].growing && Objects[i].size < 100) {
+               if (Objects[i].growing && Objects[i].size < 500) {
 
-                   Objects[i].size += Math.random() / 10
+                   Objects[i].size += Math.random()
                    
                } else if (Objects[i].growing === true) {
 
                    Objects[i].growing = false 
 
                } else if (!Objects[i].growing && Objects[i].size > 1) {
-                   Objects[i].size -= Math.random() / 10
+                   Objects[i].size -= Math.random() 
                } else {
                    Objects[i].growing = true
                }
 
                if (!Objects[i].fading && Objects[i].lightness < 70) {
 
-                    Objects[i].lightness+=Math.random() /5
+                    Objects[i].lightness+=.2
                    
                } else if (!Objects[i].fading) {
 
@@ -194,16 +200,11 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
                } else if (Objects[i].fading && Objects[i].lightness > 1) {
 
-                    Objects[i].lightness-=.7
+                    Objects[i].lightness-=.1
                    
                } else {
                     Objects[i].fading = false
                }
-
-    
-
-               
-
             
         }
 
@@ -212,15 +213,15 @@ const width = canvas.width = window.innerWidth,       //width of the canvas
 
     function create_frame() {
 
-      context.fillStyle = `hsl( ${time/3}, 100%, 30%)`;
+      context.strokeStyle = `hsl( ${time/3}, 100%, 30%)`;
         
-      context.fillRect(0,0,width,height/8)
+      context.strokeRect(0,0,width,height/8)
 
-      context.fillRect(0,0,width/8 ,height)
+      context.strokeRect(0,0,width/8 ,height)
 
-      context.fillRect(width*7/8 ,0,width,height)
+      context.strokeRect(width*7/8 ,0,width,height)
 
-      context.fillRect(width/8 , height*7/8,width,height)
+      context.strokeRect(width/8 , height*7/8,width,height)
     }
     
 
