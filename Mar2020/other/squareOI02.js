65c65715b84c4154f006da34452ce66af99c8b94
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-14 14:07:09 -0400

    more optical illusions

---
+++ Mar2020/other/squareOI02.js
@@ -0,0 +1,73 @@
+
+let canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d'),
+
+      width = canvas.width = window.innerWidth,
+      height = canvas.height = window.innerHeight,
+
+      frames = 0;
+
+
+      render()
+      function render() {
+
+        frames++
+
+        clearFullScreen()
+
+        create_squares(frames)
+
+        //   rotate_about_the_center()
+
+          setTimeout(window.requestAnimationFrame, 10, render)
+      }
+
+
+    function clearFullScreen() {
+
+        context.save();
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.restore();
+        
+    }
+
+    function create_squares(size) {
+
+        context.save()
+
+        context.translate(width/2, height/2)
+
+        for (let i = size/2; i > 0; i-= 10) {
+
+            let lightness = i/3 < 80 ? i/3 : 70;
+
+            context.beginPath()
+
+            context.rect(-i, -i, i*2, i*2)
+
+
+
+                context.strokeStyle = `hsl(${i*3}, 100%, ${lightness}%)`;
+
+            context.stroke()
+
+        }
+
+        context.restore()
+
+    }
+    
+
+    function rotate_about_the_center() {
+
+        // context.save()
+
+        context.translate(width/2, height/2)
+
+        context.rotate(.001)
+
+        context.translate(-width/2, -height/2)
+
+        // context.restore()
+    }
