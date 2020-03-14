7421e04a7dd2862fed4a94d0b1b6702c96327ccc
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-14 13:50:36 -0400

    optical illusion, I was suprised how easy this was to make, took only 10 min to figure out

---
+++ Mar2020/other/squaredepth.js
@@ -0,0 +1,56 @@
+
+let canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d'),
+
+      width = canvas.width = window.innerWidth,
+      height = canvas.height = window.innerHeight,
+
+      frames = 0;
+
+
+      render()
+      function render() {
+
+        frames++
+
+        clearFullScreen()
+
+        create_squares(frames)
+
+        //   rotate_about_the_center()
+
+          setTimeout(window.requestAnimationFrame, 10, render)
+      }
+
+
+    function clearFullScreen() {
+
+        context.save();
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.restore();
+        
+    }
+
+    function create_squares(size) {
+
+        context.save()
+
+        context.translate(width/2, height/2)
+
+        for (let i = size/2; i > 0; i-=10) {
+
+            context.beginPath()
+
+            context.rect(-i, -i, i*2, i*2)
+
+            context.strokeStyle = 'white'
+
+            context.stroke()
+
+        }
+
+        context.restore()
+
+    }
+    
