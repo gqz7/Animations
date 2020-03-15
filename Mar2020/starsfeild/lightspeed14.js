8b56073b61e77b86030dc7e1a837dc2e7e765045
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-15 06:59:55 -0400

    new animation

---
+++ Mar2020/starsfeild/lightspeed14.js
@@ -0,0 +1,245 @@
+//INITIAL VARIABLE DECLERATIONS
+
+const slider = document.getElementById('slider');
+
+let canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    time = 0;
+    
+const width = canvas.width = window.innerWidth,       //width of the canvas
+      height = canvas.height = window.innerHeight,   //height of the canvas
+      delay = 24,                                    //determins durration of time(ms) between each frame
+
+      lightSpeedStart = 260 - 200,
+
+      lightSpeedEnd = 417 - 200;
+
+  let speed = 24;                                   //sets the speed at which stars travel away from the center
+
+      context.translate(width/2, height/2) //setting the origin (0,0) to the center of the screen makes it easier to calculate where stars will spawn (will change this later so the origin can be set with a var) 
+
+
+      let Stars = []; //this array will store the values of the current stars on the screen
+
+      let tdlSmd = 0 //Total Stars Made
+
+      create_star_field() //adds stars to Stars array
+   
+    //ANIMATION CYCLE
+    
+    animate() 
+    function animate() {
+
+        time++ //a counter that counts the elapsed number of frames
+
+        if (time >= lightSpeedStart && time <= lightSpeedEnd + 300) {
+            context.rotate((time - lightSpeedStart) / 70000)
+        } 
+
+        clear()
+        
+
+        renderStars() //displays each star from its position in the Stars array 
+
+        // context.rotate(speed/10000)
+
+        moveStars(speed) //moves the position of each start slightly
+
+        setTimeout( () => {
+
+            if (time < 4444) {
+
+                if (speed < 88) {
+
+                    speed += .07
+                    
+                }
+
+                animate()
+                
+            } else {
+
+                return
+               
+            }
+
+        }, delay);
+    }
+
+    // FUNCTIONS
+
+    function create_star_streak(x, y, lightness, id) {
+
+        // context.save()
+
+        if (x < 0 ) {
+
+            x = x - 5
+            
+        } else if (x > 0) {
+
+            x = x + 5
+
+        }
+
+        if (y < 0 ) {
+
+            y = (y - 55) / 2
+            
+        } else if (y > 0) {
+
+            y = (y + 55) / 2
+
+        }
+
+        let 
+        x1 = x,
+        y1 = y,
+        x2 = lightness/100 < 0.20 ? x*(1 + lightness/100) : x*1.17,
+        y2 = lightness/100 < 0.20 ? y*(1 + lightness/100) : y*1.17,
+        
+        grad = context.createLinearGradient(x1, y1, x2, y2);
+        //set up gradient
+        grad.addColorStop(1, `hsl(0, 100%, ${lightness +5}%)`);
+        grad.addColorStop(6/7, `hsl(45, 100%, ${lightness+2}%)`);
+        grad.addColorStop(5/7, `hsl(90, 100%, ${lightness+1}%)`);
+        grad.addColorStop(4/7, `hsl(135, 100%, ${lightness}%)`);
+        grad.addColorStop(3/7, `hsl(180, 100%, ${lightness-1}%)`);
+        grad.addColorStop(2/7, `hsl(245, 100%, ${lightness-2}%)`);
+        grad.addColorStop(1/7, `hsl(305, 100%, ${lightness -5}%)`);
+        
+        context.strokeStyle = grad;
+
+        context.lineWidth = (lightness/74) + 1;
+
+        if (time >= lightSpeedStart &&  time <= lightSpeedEnd) {
+
+            // let probibility = (lightSpeedEnd - lightSpeedStart) / (lightSpeedEnd - lightSpeedStart + time);
+
+            if (id % 4 == 0) {
+
+                x1 = time - lightSpeedStart < 200 ? (x1/100) * (time - lightSpeedStart) : (x1*2)
+                y1 = time - lightSpeedStart < 200 ? (y1/100) * (time - lightSpeedStart) : (y1*2)
+                x2 = x2 * ((time - lightSpeedStart -100) / 100)
+                y2 = y2 * ((time - lightSpeedStart -100) / 100)
+
+                // context.strokeStyle = `hsl(${(Math.random()* 360)}, 100%, ${lightness-60}%)`
+                let lightspeedStroke = context.createLinearGradient(x1, y1, x2, y2),
+
+                ranColor = Math.random() * 360;
+
+                lightspeedStroke.addColorStop(2/4, `hsl(180, 100%, ${60 + lightness/10}%)`);
+                lightspeedStroke.addColorStop(1/4, `hsl(295, 100%, ${60 + lightness/10}%)`);
+                lightspeedStroke.addColorStop(0/4, `hsl(315, 100%, ${60 + lightness/10}%)`);
+                lightspeedStroke.addColorStop(3/4, `hsl(55, 100%, ${70 + lightness/10}%)`);
+
+                context.strokeStyle = lightspeedStroke;
+
+                context.lineWidth = 1;
+
+            }
+               
+        }
+        
+        //gradient line stroke 
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);       
+        context.stroke();
+
+        // context.restore()
+
+    }
+
+
+    function create_star_field() {
+
+        for (let i = 0; i < 500; i++) {
+
+            let     
+                x = (Math.random() * width) - width /2,
+                y = (Math.random() * height) - height /2,
+                size = 1,
+                lightness = 10,
+                id = ++tdlSmd;
+             
+            Stars.push({
+                x: x, y: y, size: size, lightness: lightness, id: id
+            });
+            
+        }
+        
+    }
+
+    function clear() {
+
+        context.save();
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.restore();
+        
+    }
+
+
+    function renderStars() {
+
+        for (let i = 0; i < Stars.length; i++) {
+            
+            create_star_streak(Stars[i].x, Stars[i].y, Stars[i].lightness, Stars[i].id);
+            
+        }
+
+    }
+
+    function moveStars(speed) {
+
+        for (let i = 0; i < Stars.length; i++) {
+
+            let NewX = Stars[i].x * (1 + speed/1000),
+                NewY = Stars[i].y * (1 + speed/1000);
+
+
+                if (NewX > width*5 || NewX < -width*5 || NewY > width*5 || NewY < -width*5) {
+
+                    Stars.splice(i, 1); //if it goes off screen, delete it from the stars to be rendered
+
+                    addStar() // then add a new one to replace it
+
+                    i--
+
+                } else {
+
+                    Stars[i].x = NewX;
+                    Stars[i].y = NewY;
+
+                    Stars[i].lightness += 1
+
+                }
+           
+        }
+        
+    }
+
+
+    function addStar() { //when one star dies another is born
+
+        
+        let  
+        ranNum = Math.random() * 100  
+        radius = (time/15) + 5;
+        randomX1 = (Math.cos(ranNum) * radius),
+        randomY1 = (Math.sin(ranNum) * radius),
+    
+        ranNum = Math.random(),
+
+        x =  (randomX1 / ranNum) * Math.random(),
+        y =  (randomY1 / ranNum) * Math.random(),
+
+        lightness = -speed/7,
+        id = ++tdlSmd;
+
+        Stars.push({
+            x: x, y: y, lightness: lightness, id: id
+        });
+        
+    }
