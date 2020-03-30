9aeb37c4026380675e5126424c143a31f579d136
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-29 22:14:25 -0400

    new triangle loading screen animation

--- Mar2020/tri/tri06.js
+++ Mar2020/tri/tri06.js
@@ -1,4 +1,3 @@
-//create triangle grid from origin, fractal zoom
 
 let canvas = document.getElementById('canvas'),
       context = canvas.getContext('2d'),
@@ -6,11 +5,13 @@ let canvas = document.getElementById('canvas'),
       width = canvas.width = window.innerWidth,
       height = canvas.height = window.innerHeight,
 
-      frames = 0;
+      frames = 0,
+
+      colorSelect = 144;
 
       const size = 1;
 
-      
+      context.translate(width/2, height/2)
 
         render()
 
@@ -18,38 +19,41 @@ let canvas = document.getElementById('canvas'),
 
           frames++
 
-          console.log(frames);
+          clearFullScreen()
 
-          context.save()
+          // console.log(frames);
+          let sizeChange = (frames * frames/100) + 1 < 3333 ? (frames * frames/100) + 1: false;
 
-          
-          context.translate(width/2, height/2);
-          createHex(frames*10)
-        //   createTri(size + frames*20, frames*10, frames*10)
+          if (!sizeChange) {
+            frames = 0;
+            colorSelect += 20
+          }
 
-          context.restore()
+          context.rotate(-Math.PI/777)
 
+          for (let i = sizeChange; i > 0; i-=(i/12)+3) {
+            
+            context.save()
 
-        //   context.save()
-          
-        //   context.translate(width, 0);
+            context.translate(-i/2,  i/4)
+            createTri(size + i, 0, 0, (i/7 + colorSelect), sizeChange)
 
-        //   context.rotate(Math.PI/1000)
+            context.restore()
+            
+          }
 
-        //   createTri(-size + frames*20)
-
-        //   context.restore()
-
+          // console.log(sizeChange);
      
         setTimeout(window.requestAnimationFrame, 0, render)
 
       }
 
 
-    function createTri(size, oX, oY) {
+    function createTri(size, oX, oY, color, sc) {
 
-        console.log(size, oX, oY);
-        
+        let light = 77-size/7 - (sc - 100)/50
+
+        // console.log(size, oX, oY);
 
          context.beginPath()
 
@@ -60,7 +64,8 @@ let canvas = document.getElementById('canvas'),
         context.lineTo(size,oY)
         context.lineTo(oX, oY)
 
-        context.strokeStyle = `white`;
+        context.strokeStyle = `hsl(${color}, 100%, ${light}%)`;
+        // context.strokeStyle = 'white';
 
         context.stroke()
 
@@ -73,4 +78,27 @@ let canvas = document.getElementById('canvas'),
         createTri(size,0,0)
         context.translate(size,size)
         createTri(size,0,0)
+    }
+
+    function clearFullScreen() {
+
+        context.save();
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.restore();
+        
+    }
+
+    
+    function rotate_about_the_center() {
+
+        // context.save()
+
+        context.translate(width/2, height/2)
+
+        context.rotate(.001)
+
+        context.translate(-width/2, -height/2)
+
+        // context.restore()
     }
