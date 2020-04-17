4d0dceaf2e95f57b3701d60128db6a1b07f138f7
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-04-17 00:30:41 -0400

    update readme

--- Apr2020/triangles/fractal-tri04.js
+++ Apr2020/triangles/fractal-tri04.js
@@ -1,4 +1,4 @@
-
+//VARS FOR CANVAS AND TIMING EVENTS
 let canvas = document.getElementById('canvas'),
       context = canvas.getContext('2d'),
 
@@ -7,12 +7,89 @@ let canvas = document.getElementById('canvas'),
 
       frames = width,
 
-      endSize = 150;
+      time = 0,
+      
+      pauseAnimation = false;
 
+//VARS FOR USER INPUT
+let showCircles = true,
+
+    showTriangles = false,
+
+    colorSwitch = 0,
+
+    colorIndex = 0,
+
+    endSize = 100;
+
+//USER INPUT EVENT LISTENER
+document.addEventListener('keydown', userInputEvent, false);
+
+//USER INPUT LOGIC
+function userInputEvent(input) {
+
+    switch (input.code) {
+
+        case "ShiftLeft":
+        case "ShiftRight":
+
+            context.rotate(Math.PI/6)
+
+            break;
+        case "Space":
+
+            showCircles = !showCircles;
+
+            break;
+
+         case "KeyT":
+
+            showTriangles = !showTriangles;
+
+            break;
+
+        case "KeyC":
+
+            console.log(colorSwitch);
+            
+
+            colorSwitch = colorSwitch < 3 ? colorSwitch+1 : 0;
+
+            break;
+
+        case "Comma":
+        case "ArrowLeft":
+
+            endSize = endSize > 30 ? endSize - 1 : 30;
+
+            break;
+
+        case "Period":
+        case "ArrowRight":
+
+            endSize++
+
+            break;
+
+        case "Escape":
+
+            pauseAnimation = !pauseAnimation;
+
+            if (!pauseAnimation) {
+                render()
+            }
+    
+        default:
+            break;
+    }
+    
+}
+
+//SET THE CANVAS ORIGIN TO THE MIDDLE OF THE WINDOW
       context.translate(width/2, height/2)
 
-      context.strokeStyle = 'white';
 
+//ANIMAITON CYCLE
 
         render()
 
@@ -20,7 +97,9 @@ let canvas = document.getElementById('canvas'),
 
         clearFullScreen()
 
-        endSize = endSize > 30 ? endSize - .1: 30;
+        time++
+
+        endSize = endSize > 30 ? endSize - .5: 30;
 
         frames*=1.01; 
 
@@ -28,12 +107,15 @@ let canvas = document.getElementById('canvas'),
             frames = width
         }
         createHexagon(frames/2)
- 
-        setTimeout(window.requestAnimationFrame, 3, render)
+
+        if (!pauseAnimation) {
+            setTimeout(window.requestAnimationFrame, 30, render)
+        }
 
       }
 
 
+//CREATES A TRIANGLE AT THE CANVAS ORIGIN WITH A GIVEN SIDE LENGTH 'size'
     function createTri(size) {
 
         context.beginPath()
@@ -49,6 +131,19 @@ let canvas = document.getElementById('canvas'),
 
     }
 
+//CREATES A TRIANGLE AT THE CANVAS ORIGIN WITH A GIVEN RADIUS 'size'
+
+    function createCir(size) {
+
+        context.beginPath()
+
+        context.arc(0,0,size,0,Math.PI*2)
+
+        context.stroke()
+
+    }
+
+//CLEARS THE CANVAS
     function clearFullScreen() {
 
         context.save();
@@ -58,6 +153,7 @@ let canvas = document.getElementById('canvas'),
         
     }
 
+//CREATE A HEX STRUCTURE OF SIRPINSKI TRIANGLES
     function createHexagon(size) {
 
         for (let i = 0; i < 6; i++) {
@@ -67,35 +163,53 @@ let canvas = document.getElementById('canvas'),
         
     }
 
-
+//A FUNCTION THAT USES RECURSTION TO CREATE A SIRPINSKI TRIANGLE
     function sirpinskiZoom(startSize) {
     
         if (startSize > endSize) {
             
+            setStroke(startSize)
+  
             if (startSize/2 <= endSize) {
 
-                let lWidth = (1-(endSize/startSize))*2;
-
-                context.lineWidth = lWidth;
-
-                createTri(startSize)
+                if (showTriangles) {
+                    context.lineWidth = (1-(endSize/startSize))*2;
+                    createTri(startSize)
+                }
 
             } else if (startSize/2 > endSize && startSize/2 < endSize*2) {
                 
-                context.lineWidth = 1 
+                if (showTriangles) {
+                    context.lineWidth = 1 
 
-                createTri(startSize)
-      
+                    createTri(startSize)
+                }
+
+                if (showCircles) {
+                    context.lineWidth = (1-(endSize/startSize*2))/2;
+
+                    createCir(startSize)
+                }
+
+
                 triRecursion(startSize)
 
             } else {
 
+                if (showCircles) {
+    
+                    context.lineWidth = ((endSize/startSize)*2)-.2;
+                    createCir(startSize)
+                }
+
                 triRecursion(startSize)
-            }
+
+            } 
             
         }
     }
 
+//HELP SHORTEN REPEATED CODE USED IN FUNCTION ABOVE
     function triRecursion (startSize) {
         context.save()                
         sirpinskiZoom(startSize/2)
@@ -104,4 +218,23 @@ let canvas = document.getElementById('canvas'),
         context.translate(-startSize/4, -Math.sqrt(3) * startSize * .25)
         sirpinskiZoom(startSize/2)
         context.restore();
+    }
+
+//SETS THE CONTEXT STROKE STYLE USING colorSwitch var 
+    function setStroke(size) {
+
+        switch (colorSwitch) {
+            case 0:
+
+            context.strokeStyle = 'white';
+                
+                break;
+            case 1:
+
+            context.strokeStyle = `hsl( ${time/4}, 100%, 50%)`;
+                
+            break;
+
+        }
+
     }
