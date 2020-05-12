65d114ae25fd870fe2422feaef7619084b014ac6
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-05-12 03:38:45 -0400

    first landscape animation to implement simplex noise, noise algorithm imported from library, added stars and horizon mountains for better sense of depth

--- May2020/3D/landscape04.js
+++ May2020/3D/landscape04.js
@@ -1,5 +1,8 @@
 const pi = Math.PI; //shortcut because is gets used alot
 
+//simplex noise alg import
+let Noise = toxi.math.noise.simplexNoise.noise;
+
 //i like to create all my html elements in JS so this code can be run by simplying adding it in a script tag of an empty HTML file
 let canvas = document.createElement('canvas');
     context = canvas.getContext('2d'),
@@ -24,8 +27,9 @@ let canvas = document.createElement('canvas');
         z: 0
     },
 
-    landscapePoints = []; // array to contain sphere points before they are rendered
+    landscapePoints = [], // array to contain sphere points before they are rendered
 
+    Stars = []; //array to contain star positions
     //set styling 
 
     document.body.style = 'margin: 0px;';
@@ -49,37 +53,40 @@ let canvas = document.createElement('canvas');
 
     }, false)
 
-    canvas.onmousemove = findObjectCoords;
-    // canvas.onwheel = mouseWheelMoved;
-
     document.body.style.backgroundColor = 'black';
 
     document.body.appendChild(canvas);
 
-    context.translate(0, 0)
+    context.translate(width/2,height/1.2)
 
     context.strokeStyle = 'deeppink';
     context.fillStyle = `indigo`;
 
-    context.lineWidth = .8;
+    context.lineWidth = .5;
    
    //ANIMATION CYCLE
-
-     createLandscape()
+     createStars()
      render()
 
       function render() {
-
-        //   console.log(frames);
-          
 
         clearFullScreen() //clear the canvas of previous animation cycle
 
         createLandscape() //create all the positions in an array
 
+        renderStars()
+
         renderLandscape() //render lines and shapes based on positions
 
-        frames+=10
+        if (framesUp && frames < 2077) {
+            frames+=27
+        } else if (framesUp && frames >= 2077) {
+            framesUp = false;
+        } if (!framesUp && frames > 47) {
+            frames-=23
+        } else {
+            framesUp = true;
+        }
 
         //user can toggle pausing of animation via 'spacebar'
         if (!renderPaused) {
@@ -90,61 +97,61 @@ let canvas = document.createElement('canvas');
 
     function createLandscape() {
 
-        let maxW = width/2,
-            maxH = height + (frames / 1);
+        let wlim = (width/2)/13,
+            maxH = (height) *2.62 +frames/4,
+
+            inc = frames/1000;
 
             xCount = 0;
 
             landscapePoints = [];
 
-        for (let x = 0; x < maxW; x+=17) {
+        for (let x = 0; x < wlim; x++) {
 
             landscapePoints.push([]);
 
             let yCount = 0;
             
-            for (let y = 1; y < maxH*1.5; y*=(1+ maxH/7333)) {
+            for (let y = 1; y < maxH; y*= 1.2 * (1 + frames/30000)) {
+
+                let xDis = x - 10 < 0 ? 0 : x-10;
+                    z = Noise(xCount/10, yCount/10 - inc)*(yCount*xDis/17) + xDis*5.7;
+
+                if (yCount == 0) {
+                    z = Noise(xCount, 1)*frames/50
+                }
+
                 
                 point = {
-                    x: x*(1 +((y*4)/4444)*frames/100),
+                    x: (x*13)*(1 +((y*4)/1444)),
                     y: y,
-                    z: Math.random()*(y/30)*(x/1000)
+                    z: z
                 };
 
-                rotateY(Math.PI/2.7)
-
-                landscapePoints[xCount][yCount] = point;
+                rotateY(Math.PI/2.1)
+                landscapePoints[x][yCount] = point;
 
                 yCount++
 
             }
-
             xCount++
-            
         }
-
-        // console.log(landscapePoints);
-
     }
 
     function renderLandscape() {
 
-
-        //create background 
         context.beginPath()
-
-        context.rect(0,height/2,width, height)
-
+        context.moveTo(0, -20*frames/1000)
+        context.lineTo(42*frames/1000, 42*frames/1000)
+        context.lineTo(-42*frames/1000, 42*frames/1000)
+        context.lineTo(0, -20*frames/1000)
         context.fill()
-
-        //create landscape with 3d points
-        context.save()
-
-        context.translate(width/2,height/2)
+        context.lineTo(0, 0)
+        context.stroke()
 
         for (let i = landscapePoints.length-2; i >= 0; i--) {
             
-            for (let j = landscapePoints[i].length-2; j >= 0; j--) {
+            for (let j = 0; j <= landscapePoints[i].length-2; j++) {
 
                 let p = landscapePoints[i][j];
 
@@ -152,14 +159,9 @@ let canvas = document.createElement('canvas');
                     
                     n2 = landscapePoints[i+1][j],
 
-                    n3 = landscapePoints[i+1][j+1],
+                    n3 = landscapePoints[i+1][j+1];
 
-                    light = p.y/5 < 70 ? p.y/5 : 70;
-
-
-                    context.fillStyle = `hsl(${i*14+j},100%,${light}%)`;
-                    context.strokeStyle = `hsl(${i*14+j},100%,${light}%)`;
-
+                    context.strokeStyle = 'hotpink';
 
                     context.beginPath()
                     context.moveTo(p.x, p.y)
@@ -169,7 +171,6 @@ let canvas = document.createElement('canvas');
                     context.stroke()
                     context.fill()
 
-
                     context.beginPath()
                     context.moveTo(p.x, p.y)
                     context.lineTo(n2.x, n2.y)
@@ -177,6 +178,8 @@ let canvas = document.createElement('canvas');
                     context.lineTo(p.x, p.y)
                     context.stroke()
                     context.fill()
+
+                    context.strokeStyle = 'aqua';
 
                     context.beginPath()
                     context.moveTo(-p.x, p.y)
@@ -193,14 +196,8 @@ let canvas = document.createElement('canvas');
                     context.lineTo(-p.x, p.y)
                     context.stroke()
                     context.fill()
-                
-            }
-            
+            }   
         }
-
-
-        context.restore()
-
     }
 
     //FUNCTIONS ROTATE A GIVEN POINT ABOUT THE 0,0,0 AXIS
@@ -210,22 +207,30 @@ let canvas = document.createElement('canvas');
         point.y = (y * Math.cos(radians)) + (point.z * Math.sin(radians) * -1.0);
         point.z = (y * Math.sin(radians)) + (point.z * Math.cos(radians));
     }
-    function rotateX(radians) {
-        
-        let x = point.x;
-        point.x = (x * Math.cos(radians)) + (point.z * Math.sin(radians) * -1.0);
-        point.z = (x * Math.sin(radians)) + (point.z * Math.cos(radians));
+
+    function createStars() {
+        for (let i = 0; i < 1000; i++) {
+            
+            Stars.push({x: Math.random()*width, y: Math.random()*height})
+            
+        }
     }
-    function rotateZ(radians) {
-        let x = point.x;
-        point.x = (x * Math.cos(radians)) + (point.y * Math.sin(radians) * -1.0);
-        point.y = (x * Math.sin(radians)) + (point.y * Math.cos(radians));
+
+    function renderStars() {
+        context.strokeStyle = 'white';
+        for (let i = 0; i < Stars.length; i++) {
+
+            let s = Stars[i],
+
+            x = s.x-width/2,
+            y = s.y-height/1.2 - frames/10;
+            
+            context.beginPath()
+            context.arc(x,y,.3,0,Math.PI*2);
+            context.stroke()
+            
+        }
     }
-    
-    //function used to map numbers from int into a radian range
-    function mapNumber (number, min1, max1, min2, max2) {
-        return ((number - min1) * (max2 - min2) / (max1 - min1) + min2);
-    };
 
     function clearFullScreen() {
 
@@ -234,44 +239,5 @@ let canvas = document.createElement('canvas');
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.restore();
         
-    }
-
-
-    //event listener call back functions
-
-    function mouseWheelMoved(evn) {
-        
-        let move = evn.deltaY * -7;
-
-        radius = radius + move > 50 && radius + move < height/2 ? radius + move : radius;
-        
-    }
-
-        function findObjectCoords(mouseEvent) {
-
-            let obj = canvas,
-                obj_left = 0,
-                obj_top = 0,
-                xpos,
-                ypos;
-
-        while (obj.offsetParent)
-        {
-            obj_left += obj.offsetLeft;
-            obj_top += obj.offsetTop;
-            obj = obj.offsetParent;
-        }
-        if (mouseEvent)
-        {
-            xpos = mouseEvent.pageX;
-            ypos = mouseEvent.pageY;
-        }
-        
-        xpos -= obj_left;
-        ypos -= obj_top;
-        
-        mosPos.x = xpos
-        mosPos.y = ypos
-
     }
 
