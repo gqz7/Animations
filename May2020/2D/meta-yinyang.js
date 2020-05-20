59f35cd9390ab438fb1d8e934b5c1396602c181b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-05-19 23:26:25 -0400

    new yinyang animation

---
+++ May2020/2D/meta-yinyang.js
@@ -0,0 +1,79 @@
+let canvas = document.createElement('canvas');
+    context = canvas.getContext('2d'),
+
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight,
+
+    time = 0;
+
+    document.body.style.backgroundColor = 'black';
+
+    document.body.appendChild(canvas);
+
+    context.translate(width/2, height/2)
+
+     render()
+
+      function render() {
+        time = time < 200 ? time + 1 : 200;
+
+        clearFullScreen()
+
+        yinyang(200)
+        context.rotate(.01)
+
+        setTimeout(window.requestAnimationFrame, 0, render)
+
+      }
+
+    function yinyang(radius) {
+
+        //create half white circle        
+        context.fillStyle = 'white'; 
+        context.beginPath()
+        context.arc(0,0,radius,0,Math.PI);
+        context.fill()
+        //create black outline for white half
+        context.strokeStyle = 'black'
+        context.beginPath()
+        context.arc(0,0,radius,0,Math.PI);
+        context.stroke()
+
+        //create black half circle
+        context.fillStyle = 'black';
+        context.beginPath()
+        context.arc(0,0,radius,Math.PI,Math.PI*2);
+        context.fill()
+        //create white outline for white half
+        context.strokeStyle = 'white'
+        context.beginPath()
+        context.arc(0,0,radius,Math.PI,Math.PI*2);
+        context.stroke()
+
+        //create white full circle with 1/2 radius
+        context.fillStyle = 'white'; 
+        context.beginPath()
+        context.arc(-radius/2,0,radius/2,0,Math.PI*2)
+        context.fill()
+        //create white full circle with 1/2 radius and inner circle for white 1/2 circle
+        context.fillStyle = 'black';
+        context.beginPath()
+        context.arc(-radius/2,0,radius/7,0,Math.PI*2)
+        context.arc(radius/2,0,radius/2,0,Math.PI*2)
+        context.fill()
+        //create inner circle for black 1/2 circle
+        context.fillStyle = 'white';
+        context.beginPath()
+        context.arc(radius/2,0,radius/7,0,Math.PI*2)
+        context.fill()
+  
+    }
+
+    function clearFullScreen() {
+
+        context.save();
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.restore();
+        
+    }
