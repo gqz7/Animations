a40bcd5f4449e4c1ec42bd1de7157ae4d97800cd
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-05-22 19:06:57 -0400

    completed the first version of fractal tree using objects

--- May2020/2D/fractal-tree03.js
+++ May2020/2D/fractal-tree03.js
@@ -13,38 +13,27 @@ canvas.style = `display: block;
                 left: 0px;
                 cursor: none;
                 margin:auto;
-                background-color: hsl(177,77%,17%)`;
+                background-color: hsl(177,77%,7%)`;
 
 document.body.style = `margin: 0`;
 
 
 document.body.appendChild(canvas)
 
-context.translate(width/2, height-70);
-
-context.rotate(-Math.PI)
-
-
 context.strokeStyle = "white";
 
 context.lineWidth = 2;
 
-context.beginPath();
-context.moveTo(0,100);
-context.lineTo(0,0);
-context.stroke();
 
 let startLength = 100,
     time = 0,
     pauseAnimation = true,
     timeForward = true,
-    branchAngle = Math.PI/2,
-    divisor = .82,
-    tempNoise1,
-    tempNoise2,
-    bLim = 40,
-
-    branchPos; //array of objects, all positions of the individual branches 
+    bAngl,
+    ogAgl = Math.PI / 17,
+    divisor = .72,
+    bLim = 20,
+    branchObjs = [];
 
   render()
 
@@ -52,9 +41,9 @@ let startLength = 100,
 
         clearFullScreen()
 
-        if (timeForward && time < 1000) {
-            time+=2
-        } else if (timeForward && time >= 1000) {
+        if (timeForward && time < 400) {
+            time++
+        } else if (timeForward && time >= 400) {
             timeForward = false;
         } if (!timeForward && time > 0) {
             time--
@@ -62,108 +51,81 @@ let startLength = 100,
             timeForward = true;
         }
 
-        createBackground();
+        let tempNoise = Noise(time/100,time/100+seed);
+            bAngl = ogAgl+time/1000-tempNoise/700;
 
-        startTree();
+            blim = 20 + tempNoise*1000;
 
-        // return
-
+        fractal_tree(bAngl)
+        
+        //user can toggle pausing of animation via 'spacebar'
         if (!pauseAnimation) {
-            setTimeout(window.requestAnimationFrame, 0, render);
+            setTimeout(window.requestAnimationFrame, 0, render)
         }
 
       }
 
-function startTree() {
+function fractal_tree () {
 
-    tempNoise1 = Noise(time/300,time/300+seed);
-    tempNoise2 = Noise(time/300,time/300-seed);
+    branchObjs = [];
 
-    let 
-    tempSize = time/8 + bLim-tempNoise1*3,
-    tempAngle = branchAngle-time/1000-tempNoise1/70;
+    let root = {x:0, y:0}
 
-    blim = 20 + tempNoise1*1000;
+    create_branch_objs(bAngl, time/2, root); //fills the array will all updated branch objects
 
-    branchPos = [];
-    
-    create_tree(tempAngle, tempSize, {s: {x:0, y:0} , e: {x:0, y:0} });
-    //user can toggle pausing of animation via 'spacebar'
-
-    context.save()
-
-    
-    branchPos.push({s: {x:0, y:-200} , e: {x:0, y:0}, l:200})
-    context.translate(0,200)
     render_tree()
-
-    context.restore();
-
-    // console.log(branchPos);
 
 }
 
+function create_branch_objs(agl, len, b, end) {
 
-function create_tree(angle, length, last) { 
+    let X = b.x + len * Math.cos(agl), Y = (b.y + len * Math.sin(agl));
+
+    let 
+        leftB = {
+            x1: b.x, y1: -b.y,
+            x2:X, y2:-Y
+        },
+        rightB = {
+            x1: -b.x, y1: -b.y,
+            x:-X, y:-Y
+        };
+
+    branchObjs.push(leftB)
+    branchObjs.push(rightB)
     
-    let endX = (last.e.x + length * Math.sin(angle)),//*(.5+time/400), 
-        endY = (last.e.y + length * Math.cos(angle)),//*(.5+time/400),
-        startX = last.e.x, 
-        startY = last.e.y,
-
-        branchObj = {
-        s: {x: startX, y: startY},
-        e: {x: endX, y: endY},
-        l: length
+    let newB = {
+        x: X,
+        y: Y
     }
 
-    branchPos.push(branchObj)
+    if (len > bLim) {
 
-        length *= divisor;
+        create_branch_objs(agl-bAngl, len*divisor, newB)
+        create_branch_objs(agl+bAngl, len*divisor, newB)
 
-        if(length > bLim) { //if the branch length is still large enough two more branches will be appended to the trunk
-    
-            create_tree(angle*(tempNoise1/2+.8), length, branchObj ); //the right branch is created all the way down before the left side will continue
+    } else if (!end) {
 
-            create_tree(-angle*(tempNoise2/2+.8), length, branchObj);
+        let g = mapNumber(len/bLim, divisor, 1, 0, 1);
 
-        } 
+        create_branch_objs((agl-bAngl*g), (len*divisor)*g, newB, true)
+        create_branch_objs((agl+bAngl*g), (len*divisor)*g, newB, true)
 
+    }
 }
 
 function render_tree() {
-    for (let i = 0; i < branchPos.length; i++) {
-
-        let b = branchPos[i];
-
-        let light = 100-b.l < 50 ? 50 : 100-b.l,
-        color = time/2+b.l/2;
-
-        context.strokeStyle = `hsl(${color}, 100%, ${light}%)`;
-        context.lineWidth = b.l/10;
-       
-        context.beginPath()
-        context.moveTo(-b.s.x, b.s.y)
-        context.lineTo(-b.e.x, b.e.y)
-        context.stroke()
-
-        context.beginPath()
-        context.moveTo(b.s.x, b.s.y)
-        context.lineTo(b.e.x, b.e.y)
-        context.stroke()
-
-        
-    }
-}
-
-function createBackground() {
-
-    context.fillStyle = `hsl(133, 60%, 40%)`;
-
-    context.beginPath()
-    context.rect(-width/2,80,width,-150)
-    context.fill()
-
+    context.save()
+    context.translate(width/2, height);
+    context.rotate(bAngl-Math.PI/2)
+        for (let i = 0; i < branchObjs.length; i++) {
+            const b = branchObjs[i];
+                context.beginPath()
+                context.moveTo( b.x1, b.y1)
+                context.lineTo( b.x2, b.y2)
+                context.stroke()   
+        }
+    context.restore()
 }
 
 function clearFullScreen() { 
