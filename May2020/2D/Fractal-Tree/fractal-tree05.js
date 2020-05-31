893e578ab5c1b6a4c2666f7454b8e570e38d1941
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-05-30 23:35:24 -0400

    add more branch variety

--- May2020/2D/Fractal-Tree/fractal-tree05.js
+++ May2020/2D/Fractal-Tree/fractal-tree05.js
@@ -7,7 +7,7 @@ height = canvas.height =window.innerHeight,
 
 Noise = toxi.math.noise.simplexNoise.noise;
 
-let seed = Math.random();
+let seed = Math.random()*10;
 
 canvas.style = `display: block;
         top: 0px;
@@ -63,7 +63,7 @@ clearFullScreen()
     tempNoise = Noise(time/100,time/100+seed);
     bAngl = ogAgl+time/700-tempNoise/700;
 
-    blim = 20 + tempNoise*1000;
+    blim = 2 + tempNoise*1000;
 
 // createBackground()
 fractal_tree()
@@ -100,38 +100,31 @@ function create_branch_objs(agl, len, b, end, lvl) {
     let 
         X = b.x + len * Math.cos(agl), 
         Y = (b.y + len * Math.sin(agl)),
-        sideNoise1 = Noise(counter+seed, counter+seed*1.7)*(1),
-        sideNoise2 = Noise(counter+seed+3, counter+seed*1.7+3)*(1);
+        sideNoise1 = Noise(counter/100+seed, counter/100+seed*1.7)*(1),
+        sideNoise2 = Noise(counter/100+seed+3, counter/100+seed*1.7+3)*(1);
 
     // console.log(sideNoise1, sideNoise2);
-    let side1 = sideNoise1 > 0 ? 'l' : 'r',
-          side2 = sideNoise2 > 0 ? 'r' : 'l'; 
+    let side1 = getBranchType(sideNoise1),
+        side2 = getBranchType(sideNoise2),
 
+        lastB = end ? true : false;
 
-    if (side1 == side2) {
-        
-        // console.log(true);
-
-        // side2 = side2 == 'r' ? 'l' : 'r';
-        
-    } else {
-        // console.log(false);
-        
-    }
     let 
         leftB = {
             x1: b.x, y1: -b.y,
             x2:X, y2:-Y,
             l: len,
-            side: side1,
-            num: lvl
+            branchType: side1,
+            num: lvl,
+            end: lastB
         },
         rightB = {
             x1: -b.x, y1: -b.y,
             x:-X, y:-Y,
             l: len,
-            side: side2,
-            num: lvl
+            branchType: side2,
+            num: lvl,
+            end: lastB
         };
 
     branchObjs.push(leftB)
@@ -167,46 +160,50 @@ function render_tree() {
 
 for (let i = 0; i < branchObjs.length ; i++) {
 
-    
     const b = branchObjs[i],
           length = b.l/2,
           light = 100-length < 50 ? 50 : 100-length;
 
     let curvePoint1 = {}, curvePoint2 = {};
 
-        if (b.side == 'l') {
+    let g = b.end ? mapNumber(b.l/bLim, divisor, 1, .5, 1) : 1;
 
-            curvePoint1.x = b.x1 - (length * Math.cos(90))/(4-b.num/50)
-            curvePoint1.y = b.y1 + (length * Math.sin(90))/(4-b.num/50)
+        if (b.branchType == 1) {
 
-            curvePoint2.x = b.x2 + (length * Math.cos(90))/(4-b.num/50)
-            curvePoint2.y = b.y2 - (length * Math.sin(90))/(4-b.num/50)
+            curvePoint1.x = b.x1 + (length * Math.cos(65)) * g;           
+            curvePoint1.y = b.y1 + (length * Math.sin(-45))* g;
 
-        } else {
+            curvePoint2.x = b.x2 + (length * Math.cos(65))* g;          
+            curvePoint2.y = b.y2 - (length * Math.sin(-45));
 
-            curvePoint1.x = b.x1 + (length * Math.cos(90))/(4-b.num/50)
-            curvePoint1.y = b.y1 - (length * Math.sin(90))/(4-b.num/50)
+        } else if (b.branchType == 2) {
 
-            curvePoint2.x = b.x2 - (length * Math.cos(90))/(4-b.num/50)
-            curvePoint2.y = b.y2 + (length * Math.sin(90))/(4-b.num/50)
-            
-            // curvePoint1.x = (b.x1 + b.x2/100) -(Math.pow(b.x1 - b.x2, 2)/(4.2*b.l))
-            // curvePoint1.y = b.y1 -(Math.pow(b.y1 - b.y2, 2)/(4.2*b.l))
+            curvePoint1.x = b.x1 + (length * Math.cos(65))* g;           
+            curvePoint1.y = b.y1 - (length * Math.sin(-45))* g;
 
-            // curvePoint2.x = b.x2 - (Math.pow(b.x2 - b.x1, 2)/(4.2*b.l))
-            // curvePoint2.y = (b.y1 + b.y2/100) -(Math.pow(b.y2 - b.y1, 2)/(5.2*b.l))
+            curvePoint2.x = b.x2  + (length * Math.cos(65))* g;          
+            curvePoint2.y = b.y2 + (length * Math.sin(-45))* g;
+        
+        } else if (b.branchType == 3) {
 
+            curvePoint1.x = b.x1 + (b.l * 1.1 * (1/b.num))/3 
+            curvePoint1.y = b.y1 - (b.l * 1.1 * (1/b.num))/3
 
-            // curvePoint1.x = b.x1//(b.x1 + b.l/3)/(1-(b.l/1000));
-            // curvePoint1.y = b.y1//(b.y1 - b.l/3)/(1-(b.l/1000));
+            curvePoint2.x = b.x2 - (b.l * 1.1 * (1/b.num))/3
+            curvePoint2.y = b.y2 + (b.l * 1.1 * (1/b.num))/3
+        
+        } else if (b.branchType == 4) {
 
-            // curvePoint2.x = b.x2//(b.x2 - b.l/3)/(1-(b.l/1000));
-            // curvePoint2.y = b.y2//(b.y2 + b.l/3)/(1-(b.l/1000));
+            curvePoint1.x = b.x1 - (b.l * 1.1 * (1/b.num))/3
+            curvePoint1.y = b.y1 + (b.l * 1.1 * (1/b.num))/3
 
+            curvePoint2.x = b.x2 + (b.l * 1.1 * (1/b.num))/3
+            curvePoint2.y = b.y2 - (b.l * 1.1 * (1/b.num))/3
+        
         }
 
         context.strokeStyle = `hsl(${70 + (time/2)}, 100%, ${light}%)`;
-        context.lineWidth = length/17;
+        context.lineWidth = length/7;
     
         context.beginPath()
 
@@ -257,3 +254,16 @@ function userInputEvent(input) {
 function mapNumber (number, min1, max1, min2, max2) {
     return ((number - min1) * (max2 - min2) / (max1 - min1) + min2);
 };
+
+function getBranchType(noise) {
+    
+    if (noise < -.5) {
+        return 1
+    } else if (noise < 0) {
+        return 2
+    } else if (noise < .5 ) {
+        return 3
+    } else {
+        return 4
+    }
+}
