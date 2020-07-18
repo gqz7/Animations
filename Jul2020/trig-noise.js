9883a75b78ab0a5019ffb2f2f304a09b1fba6f6d
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-18 17:29:18 -0400

    net object spins in place and warps according to simplex noise"

--- Jul2020/trig-noise.js
+++ Jul2020/trig-noise.js
@@ -5,7 +5,7 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 const Noise = toxi.math.noise.simplexNoise.noise;
-let seed = Math.random(),
+let seed = Math.random()*100,
     time = 0,
     pauseAnimation = false;
 
@@ -50,22 +50,24 @@ function userInputEvent(input) {
 
 const renderImage = () => {
 
-    let noise1 = (Noise(time/300,time/300)*130),
-        noise2 = Noise((time+777)/300,(time+777)/300)*130,
-        noise3 = Noise((time+333)/300,(time+333)/300)*130,
-        noise4 = Noise((time+444)/300,(time+444)/300)*130;
+    const base = seed + time;
 
-    context.translate((noise1-noise3)/100, (noise2-noise4)/100)
+    let noise1 = Noise((base+111)/300,(base+111)/300)*130,
+        noise2 = Noise((base+777)/300,(base+777)/300)*130,
+        noise3 = Noise((base+333)/300,(base+333)/300)*130,
+        noise4 = Noise((base+444)/300,(base+444)/300)*130;
+
+    // context.translate((noise3-noise1)/10, (noise2-noise4)/10)
 
     const mult = 1//.1+time/1000;
         
     for (let i = 0; i < Math.PI*2; i+=Math.PI/35) {
 
         const 
-            x1 =  sin(i) * (height/(7*(1+noise1/373))) ,
+            x1 =  sin(i) * (width/(7*(1+noise1/373))) ,
             x2 =  noise2,
             y1 =  noise3,
-            y2 =  cos(i) * (height/(7*(1+noise4/373)));
+            y2 =  cos(i) * (width/(7*(1+noise4/373)));
 
         let 
             color = (noise1/289)+(time*3)+(Math.abs(x1, y2)*1);
