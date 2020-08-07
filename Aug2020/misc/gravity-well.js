806eb9e7e8d4de9867a622b6baa921d9874a5570
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-07 03:19:11 -0400

    distorted noise field

--- Aug2020/misc/gravity-well.js
+++ Aug2020/misc/gravity-well.js
@@ -1,10 +1,21 @@
 //this file uses the simplex noise algorithm from a library
-
+// alert('CONTROLS\nPress S to toggle frame screen clear\nPress Space to ( Pause / Play ) animation\nUse O & L to toggle displaying of lines/dots\nUse Up and Down Arrow Keys to change speed of animation')
 const Noise = toxi.math.noise.simplexNoise.noise,
       pi = Math.PI,
       sqrt = Math.sqrt,
-      pow = Math.pow;
-let seed = Math.random()*100;
+      pow = Math.pow,
+      limits = {
+        sX: 20,
+        eX: 60,
+        sY: 3,
+        eY: 41,
+    };
+
+let seed = Math.random()*10000,
+    mosPos = {
+        x: 1000,
+        y: 1000,
+    };
 //VARS FOR CANVAS AND TIMING EVENTS
 let canvas = document.createElement('canvas'),
       context = canvas.getContext('2d'),
@@ -13,9 +24,14 @@ let canvas = document.createElement('canvas'),
       time = 0,
       timeMax = Infinity,
       timeForward = true,
-      speed = .37,
+      speed = .5,
       clearScreen = true,
-      pauseAnimation = false;
+      pauseAnimation = false,
+      showLines = true,
+      showDots = true,
+      isInColor = true,
+      blockSize = 24,
+      colorMult = 1.7;
 
 context.strokeStyle = 'white';
 context.fillStyle = 'white';
@@ -36,6 +52,7 @@ document.body.appendChild(canvas)
 
 //USER INPUT EVENT LISTENER
 document.addEventListener('keydown', userInputEvent, false);
+canvas.onmousemove = findObjectCoords;
 
 //USER INPUT LOGIC
 function userInputEvent(input) {
@@ -47,11 +64,33 @@ function userInputEvent(input) {
             }
         break;
         case 'ArrowUp':
-            speed = speed < 1 ? speed+.1 : 1;
+            speed = speed < 4 ? speed+.1 : 4;
         break;
         case "ArrowDown":
-            speed = speed > .2 ? speed-.1 : .2;
-    }
+            speed = speed > .1 ? speed-.1 : .1;
+        break;
+        case 'ArrowLeft':
+            colorMult = colorMult > .5 ? colorMult-.3 : .5;
+        break;
+        case "ArrowRight":
+            colorMult = colorMult < 5 ? colorMult+=.1 : 5;
+          break;
+        case "KeyL":
+            showLines = !showLines;
+            if (!showDots && !showLines) showDots = true
+        break;
+        case "KeyO":
+            showDots = !showDots;
+            if (!showDots && !showLines) showLines = true
+        break;
+        case "KeyS":
+            clearScreen = !clearScreen;
+        break;
+        case "KeyC":
+            isInColor = !isInColor;
+        break;
+        }
+        
 }
 
 //SET THE CANVAS ORIGIN TO THE MIDDLE OF THE WINDOW
@@ -76,44 +115,257 @@ function userInputEvent(input) {
         }
 
         if(clearScreen) clearFullScreen()
-            
+
         createImg(time)
+        // renderMouse()
         
         if (!pauseAnimation) {
-            setTimeout(window.requestAnimationFrame, 0, render)
+            setTimeout(window.requestAnimationFrame, 10, render)
         }
       }
 
 function createImg(s) { 
 
-    context.save();
+    const points = [];
+    const timeNoise = Math.abs(Noise(s/1000 + seed, s/1000 + seed)*1.5);
 
-        for (let x = -10; x < width/20+10; x++) {
+        for (let x = limits.sX; x < limits.eX; x++) {
+
+            points.push([]);
             
-            for (let y = -10; y < height/20+10; y++) {
-
-                const
-                noiseX = x/30 + s/50, 
-                noiseY = y/30,
-                distance = sqrt( pow((x*20)-(width/2), 2) +  pow((y*20)-(height/2), 2) ),
-                N1 = Noise(noiseX, noiseY)*distance/100,
-                N2 = Noise(noiseY,noiseX)*distance/100,
-                radius = 2//+Math.abs(2*N1);
-                X = x*20+ N1*20+N2*20,
-                Y = y*20+ N1*20-N2*20;
-
-                context.fillStyle = `hsl(${0}, 0%, ${distance/5}%)`;
+            for (let y = limits.sY; y < limits.eY; y++) {
                 
-                context.beginPath()
-                context.arc(X, Y, radius, 0, pi*2)
-                context.fill()
+                const
+                noiseX = x/19+timeNoise + s/77, 
+                noiseY = y/19+timeNoise + s/77,
+                distance = sqrt( pow((x*blockSize)-(width/2), 2) +  pow((y*blockSize)-(height/2), 2) ),
+                N1 = Noise(noiseX, noiseY+seed)*(3.6 -pow(distance,.77)/28),
+                N2 = Noise(noiseY+seed,noiseX)*(3.6 -pow(distance,.77)/28),
+                X = x*blockSize+ N1*20+N2*20,
+                Y = y*blockSize+ N1*20-N2*20,
+                noise = Math.abs(N1 + N2)/2 +( 1 * distance/7000),
+                point = {x: X, y: Y, n: noise};
+
+                points[x-limits.sX].push(point)
                         
             }
             
         }
 
-    context.restore();
+        renderCanvasBG()
+        renderPoints(points)
+        // renderBorder()
 
+}
+
+function renderCanvasBG() {
+    const backgroundColor =  `#111`
+
+    context.fillStyle = backgroundColor;
+
+    //limits.sX*blockSize, limits.sY*blockSize, limits.eX*blockSize, limits.eY*blockSize
+    context.rect(
+        limits.sX*blockSize, 
+        limits.sY*blockSize, 
+        limits.eX*blockSize-blockSize-limits.sX*blockSize, 
+        limits.eY*blockSize-blockSize-limits.sY*blockSize
+    );
+    context.fill()
+}
+
+function renderBorder() {
+    const borderColor = '#222';
+
+    context.fillStyle = borderColor;
+
+    //limits.sX*blockSize, limits.sY*blockSize, limits.eX*blockSize, limits.eY*blockSize
+    context.rect(
+        limits.sX*blockSize-blockSize, 
+        limits.sY*blockSize-blockSize, 
+        limits.eX*blockSize-blockSize-limits.sX*blockSize+blockSize*2, 
+        blockSize
+    );
+    context.fill()
+
+    context.rect(
+        limits.sX*blockSize-blockSize, 
+        limits.eY*blockSize-blockSize, 
+        limits.eX*blockSize-blockSize-limits.sX*blockSize+blockSize*2, 
+        blockSize
+    );
+    context.fill()
+
+    context.rect(
+        limits.sX*blockSize-blockSize, 
+        limits.sY*blockSize-blockSize, 
+        blockSize,
+        limits.eY*blockSize-blockSize-limits.sY*blockSize+blockSize*2
+    );
+    context.fill()
+
+    context.rect(
+        limits.eX*blockSize-blockSize, 
+        limits.sY*blockSize-blockSize, 
+        blockSize,
+        limits.eY*blockSize-blockSize-limits.sY*blockSize+blockSize*2
+    );
+    context.fill()
+
+}
+
+function renderMouse() {
+
+    const originX = mosPos.x;
+    const originY = mosPos.y;
+
+    const noise1 = (Noise(seed+time/100, seed+time/100));
+    const noise2 = (Noise(seed+0.4140+time/100, seed+0.4140+time /100));
+
+    context.fillStyle = 'yellow';                  
+    context.beginPath()
+    context.arc(originX+(noise2)*12.7, originY+(noise1)*12.7, 1.5, 0, pi*2)
+    context.fill()
+
+    const noise3 = (Noise(seed+333+time/100, seed+333+time/100));
+    const noise4 = (Noise(seed+0.41400+time/100, seed+0.41400+time /100));
+
+    context.fillStyle = 'magenta';                  
+    context.beginPath()
+    context.arc(originX+(noise3)*12.7, originY+(noise4)*12.7, 1.5, 0, pi*2)
+    context.fill()
+
+    const noise5 = (Noise(seed+777+time/100, seed+777+time/100));
+    const noise6 = (Noise(seed+510+time/100, seed+510+time /100));
+
+    context.fillStyle = 'aqua';                  
+    context.beginPath()
+    context.arc(originX+(noise5)*12.7, originY+(noise6)*12.7, 1.5, 0, pi*2)
+    context.fill()
+
+}
+
+function renderPoints(arr) {
+
+    // const t = Math.ceil(time/20)
+    const saturation = isInColor ? 100 : 0;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        for (let j = 0; j < arr[i].length; j++) {
+            
+            const p = arr[i][j],
+            pColor = calcColor(p.n)
+
+            if (showLines) {
+                const px = 
+                    arr[i+1] != undefined 
+                    ? arr[i+1][j]   
+                    : false;
+                const py = 
+                    arr[i][j+1] != undefined 
+                    ? arr[i][j+1]   
+                    : false;
+                const pxy = 
+                    arr[i+1] != undefined 
+                    && arr[i+1][j+1] != undefined 
+                    ? arr[i+1][j+1] 
+                    : false;
+                const pm = 
+                    px
+                    && py
+                    && pxy
+                    ? {
+                        n: (p.n + pxy.n + px.n + py.n)/4,
+                        x: p.x + blockSize/2,
+                        y: p.y + blockSize/2
+                    }
+                    : false;
+
+                if (pm && py) {
+                    const
+                    pColor = calcColor((pm.n+p.n+py.n)/3);
+        
+                    context.fillStyle = pColor;                  
+                    context.beginPath()
+                    context.moveTo(p.x, p.y)
+                    context.lineTo(py.x,py.y)
+                    context.lineTo(pm.x,pm.y)
+                    context.lineTo(p.x, p.y)
+                    context.fill()
+
+                }
+
+                if (pm && px) {
+                    const 
+                    pColor = calcColor((pm.n+p.n+px.n)/3);
+        
+                    context.fillStyle = pColor;                  
+                    context.beginPath()
+                    context.moveTo(p.x, p.y)
+                    context.lineTo(px.x,px.y)
+                    context.lineTo(pm.x,pm.y)
+                    context.lineTo(p.x, p.y)
+                    context.fill()
+
+                }
+
+                if (pm && pxy && px) {
+                    const 
+                    pColor = calcColor((pm.n+px.n+pxy.n)/3);
+        
+                    context.fillStyle = pColor;                  
+                    context.beginPath()
+                    context.moveTo(px.x, px.y)
+                    context.lineTo(pxy.x,pxy.y)
+                    context.lineTo(pm.x,pm.y)
+                    context.lineTo(px.x, px.y)
+                    context.fill()
+
+                }
+
+                if (pm && pxy && py) {
+                    const 
+                    pColor = calcColor((pm.n+py.n+pxy.n)/3);
+        
+                    context.fillStyle = pColor;                  
+                    context.beginPath()
+                    context.moveTo(py.x, py.y)
+                    context.lineTo(pxy.x,pxy.y)
+                    context.lineTo(pm.x,pm.y)
+                    context.lineTo(py.x, py.y)
+                    context.fill()
+
+                }
+                if (pm && showDots) {
+                    const 
+                    pColor = calcColor(pm.n);
+        
+                    context.fillStyle = pColor;                  
+                    context.beginPath()
+                    context.arc(pm.x, pm.y, 5, 0, pi*2)
+                    context.fill()
+                }
+
+            }
+
+            if (showDots) {
+                context.fillStyle = pColor;                  
+                context.beginPath()
+                context.arc(p.x, p.y, 5, 0, pi*2)
+                context.fill()
+            }
+
+
+        }
+    }
+
+}
+
+function calcColor(n) {
+    const light = (100*n) < 90 ? (100*n) : 90,
+          sat = n*100,
+          color = (n * 360 * colorMult) + time*3;
+    return `hsl(${color}, ${sat}%, ${light}%)`
 }
 
 function clearFullScreen() {
@@ -128,3 +380,32 @@ function clearFullScreen() {
 function mapNumber (number, min1, max1, min2, max2) {
     return ((number - min1) * (max2 - min2) / (max1 - min1) + min2);
 };
+
+
+function findObjectCoords(mouseEvent) {
+
+    let obj = canvas,
+        obj_left = 0,
+        obj_top = 0,
+        xpos,
+        ypos;
+
+while (obj.offsetParent)
+{
+    obj_left += obj.offsetLeft;
+    obj_top += obj.offsetTop;
+    obj = obj.offsetParent;
+}
+if (mouseEvent)
+{
+    xpos = mouseEvent.pageX;
+    ypos = mouseEvent.pageY;
+}
+
+xpos -= obj_left;
+ypos -= obj_top;
+
+mosPos.x = xpos
+mosPos.y = ypos
+
+}
