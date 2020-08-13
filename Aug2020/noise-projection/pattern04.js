46ef9a9237243c8283aef0fd366468c84b683159
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-13 16:06:57 -0400

    update for rendering long animation

--- Aug2020/noise-projection/pattern04.js
+++ Aug2020/noise-projection/pattern04.js
@@ -1,6 +1,6 @@
 const Noise = toxi.math.noise.simplexNoise.noise;
-let seed1 = Math.random()*100;
-let seed2 = Math.random()*100;
+let seed1 = (Math.random()*300);
+let seed2 = (Math.random()*400);
 
 // alert('CONTROLS\nPress E to adjust object orientation\nPress S to toggle frame screen clear\nPress Space to ( Pause / Play ) animation\nUse T & Y to cycle through the diffrent animation variariations')
 //VARS FOR CANVAS AND TIMING EVENTS
@@ -10,8 +10,8 @@ let canvas = document.createElement('canvas'),
       height = canvas.height = window.innerHeight,
       strokeW = 1,
       speed = .01,
-      pauseAnimation = false,
-      viewWidth = width/3;
+      pauseAnimation = true,
+      viewWidth = width/2;
 
 // context.strokeStyle = 'white';
 // context.fillStyle = 'white';
@@ -24,7 +24,7 @@ canvas.style = `display: block;
                 left: 0px;
                 // cursor: none;
                 margin:auto;
-                background-color: black`;
+                background-color: white`;
 
 document.body.style = `margin: 0`;
 
@@ -52,67 +52,53 @@ function userInputEvent(input) {
 }
 
 //SET THE CANVAS ORIGIN TO THE MIDDLE OF THE WINDOW
-    context.translate(width/2.4, height/2)   
+    context.translate(width/3+80, height/2)   
     context.rotate(Math.PI/2)
 
 //ANIMAITON CYCLE
 render()
 
 function render() {
-
     seed1+=speed;
     seed2+=speed;
     // clearFullScreen()
     renderNoise()
-
+    // return
     if (!pauseAnimation) {
         setTimeout(window.requestAnimationFrame,  0, render)
     }
 }
-
 function renderNoise() {
-
-    const maxH =70;
-
-    viewWidth = 100;
-    const vertRes = .2;
-
-    for (let i = 1; i < maxH; i+=vertRes) {
-
+    const maxH =141,  vertRes = .2;
+    for (let i = 7.6; i < maxH; i+=vertRes) {
         context.save()
         context.translate(-i*3.5+30,-viewWidth/4)
-        createImg(-i, maxH, 1)
+        createImg(-i, maxH, seed1)
         context.translate((i*4-30)*3.5/2,0)
-        createImg(-i, maxH, 2)
+        createImg(-i, maxH, seed2)
         context.restore()
     }
 }
 
-function createImg(s, max, num) { 
-    const nRes = s,
-          seed = num === 1 ? seed1 : seed2;
-
-    for (let y = 0; y < viewWidth; y++) {
-
-          const offX = (mapNumber(nRes, 12+(s*Math.abs(1/100))/3.4, max, 0, width*2)/nRes/4.2),
-                offY = (mapNumber(nRes, (1+s/400)*(1+y*1/s*.1), max, 0, height*2)/nRes/1.2),
-                noiseX = ((1*10)/(nRes*20))+offX+seed,
-                noiseY = ((y*7)/(nRes*11.7))+offY-seed/3,
-                divid = s/1.2 < 20 ? s/1.2 : 20,
-                light = ((Math.abs((Noise(noiseX, noiseY)*divid/2 )) % divid)+(nRes/1.37)+67),
-                color = (Math.abs((Noise(noiseX, noiseY)*divid ) % 10)*divid)+s*3-60;
-                
-                context.strokeStyle = `hsl(${color}, 40%, ${light}%)`; //Math.random()*100
-
-                context.beginPath()
-                    context.moveTo(1,y)
-                    context.lineTo(2,y)
-                    
-                    context.moveTo(1,-y)
-                    context.lineTo(2,-y)
-                context.stroke()
+function createImg(nRes, max, seed) { 
+    for (let y = .5; y < viewWidth; y++) {
+          const offX = (mapNumber(nRes, 12+(nRes/900)/3.4, max, 0, width*2)/nRes/4.2),
+                offY = (mapNumber(nRes, (1+nRes/5000)*(1+y*1/nRes*.071), max, 0, height*2)/nRes/1.2),
+                noiseX = ((1*10)/(nRes*13))-offX/2+seed,
+                noiseY = ((y*7)/(nRes*19))-offY/13+seed/3,
+                divid = nRes/1.2 < 20 ? nRes/1.2 : 20,
+                light = ((Math.abs(( Noise(noiseX, noiseY)*divid/2 )) % divid)+(nRes/1.37)+70)+((y/max*90)/7.7);
+                if (light < 98) {
+                    const color = (Math.abs((Noise(noiseX, noiseY)*divid ) % 10)*divid/1.2)+nRes*2-60;
+                    context.strokeStyle = `hsl(${color}, 50%, ${light}%)`; //Math.random()*100
+                    context.beginPath()
+                        context.moveTo(1,y)
+                        context.lineTo(2,y)
+                        context.moveTo(1,-y)
+                        context.lineTo(2,-y)
+                    context.stroke()
+                }
     }
-
 }
 
 
