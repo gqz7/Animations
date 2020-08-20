f6f4f388f4c56c79c29f9c891dd804941e97068b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-20 03:35:01 -0400

    improve user controls and clean up some code

--- Aug2020/misc/merkabah.js
+++ Aug2020/misc/merkabah.js
@@ -1,4 +1,4 @@
-// alert('\nControls:\n\nSpace To Pause\n\nA to toggle auto-rotation\n\nW to toggle visibilty of lines that pass through center point')
+alert('\nControls:\n\nSpace To Pause\n\nA to toggle auto-rotation\n\nW to toggle visibility of lines that pass through center point\n\nQ to toggle mouse based object rotation\n\nE to toggle visibility of points')
 
 const pi = Math.PI; //shortcut because is gets used alot
 
@@ -10,11 +10,13 @@ let canvas = document.createElement('canvas');
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight,
 
-    frames = 0, //keep count of how many render cycles have occured
+    frames = 1000, //keep count of how many render cycles have occured
 
-    renderPaused = false,  //user can toggle animation
-    autoRotate = true,    //roates z axis, can be toggle by user
-    hideMidLines = true, //detirmins if lines through center are shown in render
+    renderPaused = false,   //user can toggle animation
+    autoRotate = false,     //roates z axis, can be toggle by user
+    mouseRotate = false,  //determines if the user can rotate the merkabah my moving the mouse on the canvas
+    hideMidLines = true, //determines if lines through center are shown in render
+    showPoints = false, //determines if the points of the merkabah will show
 
     mosPos = {
         x: width/2,
@@ -38,21 +40,28 @@ let canvas = document.createElement('canvas');
     //event listener for user input
     document.addEventListener('keydown', (evn) => {
 
-        if (evn.code == 'Space') {
-
-            renderPaused = !renderPaused;
-        
-            if (!renderPaused) { 
-                render()
-            }
+        switch (evn.code) {
+            case 'Space':
+                    renderPaused = !renderPaused;
             
-        } else if (evn.code == 'KeyA') {
-
-            autoRotate = !autoRotate;
-            
-        } else if (evn.code == 'KeyW') {
-
-            hideMidLines = !hideMidLines;
+                    if (!renderPaused) { 
+                        render()
+                    }
+                break;
+            case 'KeyA':
+                    autoRotate = !autoRotate;
+                    mouseRotate = false;
+                break;
+            case 'KeyQ':
+                    mouseRotate = !mouseRotate;
+                    autoRotate = false;
+                break;
+            case 'KeyW':
+                    hideMidLines = !hideMidLines;
+                break;
+            case 'KeyE':
+                    showPoints = !showPoints;
+                break;
         }
 
     }, false)
@@ -65,39 +74,26 @@ let canvas = document.createElement('canvas');
 
     context.fillStyle = 'white';
     context.strokeStyle = 'white';
-   
    //ANIMATION CYCLE
-     render()
-
-      function render() {
-
-        // console.log(frames);
-
-        clearFullScreen() //clear the canvas of previous animation cycle
-        calcPoints()
-        createMerkabah() 
-
-        //counts how many frames have occured
-        frames++
-
-        //user can toggle pausing of animation via 'spacebar'
-        if (!renderPaused) {
-            setTimeout(window.requestAnimationFrame, 0, render)
-        }
-
-      }
-
+    render()
+    function render() {
+    clearFullScreen() //clear the canvas of previous animation cycle
+    calcPoints()
+    createMerkabah() 
+    //counts how many frames have occured
+    frames++
+    //user can toggle pausing of animation via 'spacebar'
+    if (!renderPaused) {
+        setTimeout(window.requestAnimationFrame, 0, render)
+    }
+    }
     //function used to map numbers from int into a radian range
     function mapNumber (number, min1, max1, min2, max2) {
         return ((number - min1) * (max2 - min2) / (max1 - min1) + min2);
     };
-
     function createMerkabah() {
 
         const points = [];
-
-        // let count = 0;
-
         merkabahPoint.forEach(p => {
             // count++
             point = {
@@ -106,27 +102,27 @@ let canvas = document.createElement('canvas');
                 z: p.z,
                 c: p.c 
             }
-            
-            let xRotation = mosPos.x/111,
-            yRotation = mosPos.y/111;
-        
             //rotate the points to give the illusion of 3d
-            rotateX(xRotation)
-            rotateY(yRotation)
-            rotateZ(pi/12)
-
-            if (autoRotate) {
+            // rotateZ(pi/12)
+            
+            if (mouseRotate) {
+                let 
+                xRotation = mosPos.x/111,
+                yRotation = mosPos.y/111;
+                rotateX(xRotation)
+                rotateY(yRotation)
+            } else if (autoRotate) {
                 rotateZ(frames/222)
                 rotateX(frames/222)
                 rotateY(frames/222)
+            } else  {
+                rotateZ(2.1)
+                rotateY(.75)
+                rotateX(2.2)
             }
-
             points.push(point)
-
         });
-        
         renderMerkabah(points)
-        
     }
     
     function renderMerkabah (array) {
@@ -151,12 +147,12 @@ let canvas = document.createElement('canvas');
                 
             });
         }
-        for (let i = 0; i < array.length; i++) {
-            const p = array[i];
-
-            // renderPoint(p, maxZ)
+        if (showPoints) {
+            for (let i = 0; i < array.length; i++) {
+                const p = array[i];
+                renderPoint(p, maxZ)
+            }
         }
-
     }
 
     function calcPoints() {
@@ -173,8 +169,6 @@ let canvas = document.createElement('canvas');
             {x: -size2, y: -size1, z: size1,  c: 225},
             {x: size1,  y: -size2, z: -size1, c: 270},
             {x: -size1, y: size1,  z: -size2, c: 315},
-        
-            // {x: 0, y:0, z: 0, c: 123}, //centerpoint
         
         ]        
 
@@ -199,19 +193,19 @@ let canvas = document.createElement('canvas');
 
     function renderLine(start, end, mz) {
 
-        const segs = 44;//number of line segments that make up one line
+        const sgs = 37;//number of line segments(sgs) that make up one line
 
-        for (let i = 0; i < segs; i++) {
+        for (let i = 0; i < sgs; i++) {
 
-            startX = mapNumber(i/segs, 0, segs/i, start.x, end.x)
-            startY = mapNumber(i/segs, 0, segs/i, start.y, end.y)
-            endX = mapNumber((i+1)/segs, 0, segs/(i+1), start.x, end.x)
-            endY = mapNumber((i+1)/segs, 0, segs/(i+1), start.y, end.y)
+            startX = mapNumber(i/sgs, 0, sgs/i, start.x, end.x)
+            startY = mapNumber(i/sgs, 0, sgs/i, start.y, end.y)
+            endX = mapNumber((i+1)/sgs, 0, sgs/(i+1), start.x, end.x)
+            endY = mapNumber((i+1)/sgs, 0, sgs/(i+1), start.y, end.y)
 
             const Z = (start.z + end.z)/2;
             const alpha = mapNumber(-Z, -mz, mz, .05, 1);
 
-            const color = mapNumber(i, 0, segs, 0, 360)+frames*2;
+            const color = mapNumber(i, 0, sgs, 0, 360)+frames*2;
             
             context.strokeStyle = `hsl(${color}, 100%, 50%, ${alpha})`
 
@@ -243,7 +237,6 @@ let canvas = document.createElement('canvas');
         point.x = (x * Math.cos(radians)) + (point.y * Math.sin(radians) * -1.0);
         point.y = (x * Math.sin(radians)) + (point.y * Math.cos(radians));
     }
-
 
     function clearFullScreen() {
 
@@ -277,9 +270,7 @@ let canvas = document.createElement('canvas');
         xpos -= obj_left;
         ypos -= obj_top;
         
-        mosPos.x = xpos
-        mosPos.y = ypos
+        mosPos.x = xpos;
+        mosPos.y = ypos;
 
     }
-
- 
