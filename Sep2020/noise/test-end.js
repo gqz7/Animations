adc2a69447981f40b2f616d0e38dc0705247dd79
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-04 17:26:25 -0400

    testing noise project

---
+++ Sep2020/noise/test-end.js
@@ -0,0 +1,149 @@
+const Noise = toxi.math.noise.simplexNoise.noise;
+let seed1 = (Math.random()*300)+3333;
+let seed2 = (Math.random()*400)+7777;
+
+// alert('CONTROLS\nPress E to adjust object orientation\nPress S to toggle frame screen clear\nPress Space to ( Pause / Play ) animation\nUse T & Y to cycle through the diffrent animation variariations')
+//VARS FOR CANVAS AND TIMING EVENTS
+let canvas = document.createElement('canvas'),
+      context = canvas.getContext('2d'),
+      width = canvas.width = window.innerWidth,
+      height = canvas.height = window.innerHeight,
+      strokeW = 1,
+      speed = .01,
+      pauseAnimation = true,
+      viewWidth = width/2;
+
+// context.strokeStyle = 'white';
+// context.fillStyle = 'white';
+
+// context.lineWidth = strokeW;
+
+canvas.style = `display: block;
+                position: static;
+                top: 0px;
+                left: 0px;
+                // cursor: none;
+                margin:auto;
+                background-color: black`;
+
+document.body.style = `margin: 0`;
+
+document.body.appendChild(canvas)
+
+//USER INPUT EVENT LISTENER
+document.addEventListener('keydown', userInputEvent, false);
+
+//USER INPUT LOGIC
+function userInputEvent(input) {
+    switch (input.code) {
+
+        case 'Space':
+            pauseAnimation = !pauseAnimation;
+            if (!pauseAnimation) {
+                render()
+            }
+        break;
+        case 'ArrowUp':
+            speed = speed < 1 ? speed+.1 : 1;
+        break;
+        case "ArrowDown":
+            speed = speed > .003 ? speed-.1 : .003;
+    }
+}
+
+//SET THE CANVAS ORIGIN TO THE MIDDLE OF THE WINDOW
+    context.translate(width/3+80, height/2)   
+    context.rotate(Math.PI/2)
+
+//ANIMAITON CYCLE
+render()
+
+function render() {
+    seed1-=speed;
+    seed2-=speed;
+    // clearFullScreen()
+    renderNoise()
+    // return
+    if (!pauseAnimation) {
+        setTimeout(window.requestAnimationFrame,  0, render)
+    }
+}
+function renderNoise() {
+    const maxH =131,  vertRes = .2;
+    for (let i = 7.6; i < maxH+80; i+=vertRes) {
+        context.save()
+        context.translate(-i*3.5+30,-viewWidth/4)
+        createImg(-i, maxH, seed1)
+        context.translate((i*4-30)*3.5/2,0)
+        createImg(-i, maxH, seed2)
+        context.restore()
+    }
+}
+
+function createImg(nRes, max, seed) { 
+    let yCount = 0;
+    for (let y = 0; y < viewWidth; y++) {
+          const offX = (mapNumber(nRes, 12+(nRes/900)/3.4, max, 0, width*2)/nRes/4.2),
+                offY = (mapNumber(nRes, (1+nRes/5000)*(1+y*1/nRes*.071), max, 0, height*2)/nRes/1.2),
+                noiseX = ((1*10)/(nRes*13))-offX/2+seed,
+                noiseY = ((y*7)/(nRes*19))-offY/13+seed/3,
+            divid = nRes/1.3 < 54 ? nRes/1.3 : 1,
+            // divid = nRes/1.2 < 20 ? nRes/1.2 : 20,
+                light = ((Math.abs(( Noise(noiseX, noiseY)*divid/2 )) % divid)+(nRes/2.3)+70)-((y/max*67)/8);
+                if (light > 1) {
+                const color =  Math.abs(Noise(noiseX, noiseY))*(divid*divid)+nRes*1-127+y/3;
+
+                    context.strokeStyle = `hsl(${color}, 50%, ${light}%)`; //Math.random()*100
+                    context.beginPath()
+                        context.moveTo(0,y)
+                        context.lineTo(1,y)
+                        context.moveTo(0,-y)
+                        context.lineTo(1,-y)
+                    context.stroke()
+                }
+
+            // const 
+
+            // time = nRes,
+            // transX = nRes == 77 ? s/30 : 0,
+            // x = nRes,
+            // xCount = nRes,
+            // s= nRes,
+
+            // offX = (mapNumber(nRes, 12+(time*Math.abs(x/100))/1.4, max, 0, width*2.7-s*2)/nRes/4.2)+transX/9,
+            // offY = (mapNumber(nRes, (1+time/400)*(1+y*x/time*.1), max, 0, height*2.7-s*2)/nRes/1.2),
+            // noiseX = ((xCount*10)/(nRes*3))+offX+seed,
+            // noiseY = ((yCount*10)/(nRes*19.7))+offY+seed,
+            // divid = time/1.3 < 54 ? time/1.3 : 54,
+            // light = ((Math.abs((Noise(noiseX, noiseY)*divid )) % divid)+(nRes/5.33)+5)-y/24+10;
+
+            // if (light > 1) {
+            //     const color =  (Math.abs((Noise(noiseX, noiseY) ))*Math.pow(divid,1.9))+s*1-127+y/1.3;
+            //     context.strokeStyle = `hsl(${color}, 40%, ${light}%)`; //Math.random()*100
+               
+            //     console.log(color);
+            //     context.beginPath()
+            //         context.moveTo(1,y)
+            //         context.lineTo(2,y)
+            //         context.moveTo(1,-y)
+            //         context.lineTo(2,-y)
+            //     context.fill()
+                
+            // }
+            // yCount++
+    }
+}
+
+
+function clearFullScreen() {
+
+    context.save();
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.restore();
+    
+}
+
+function mapNumber (number, min1, max1, min2, max2) {
+    return ((number - min1) * (max2 - min2) / (max1 - min1) + min2);
+};
