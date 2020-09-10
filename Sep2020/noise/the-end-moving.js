fa3630d25fb7fca2bbdae928bcd79f7adbc2189e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-09 23:05:43 -0400

    update animation

--- Sep2020/noise/the-end-moving.js
+++ Sep2020/noise/the-end-moving.js
@@ -1,7 +1,7 @@
 
 const Noise = toxi.math.noise.simplexNoise.noise;
-let seed1 = 95.40114300303621// Math.random()*100;
-let seed2 = 8.222677424127378//Math.random()*100;
+let seed1 = 417.3939// Math.random()*100;
+let seed2 = 3141.5826//Math.random()*100;
 
 // 3.7866918545821027
 // seed 2: 18.83535817399713
@@ -10,98 +10,97 @@ console.log(`seed 1: ${seed1}\nseed 2: ${seed2}\n`);
 // alert('CONTROLS\nPress E to adjust object orientation\nPress S to toggle frame screen clear\nPress Space to ( Pause / Play ) animation\nUse T & Y to cycle through the diffrent animation variariations')
 //VARS FOR CANVAS AND TIMING EVENTS
 let canvas = document.createElement('canvas'),
-      context = canvas.getContext('2d'),
+			context = canvas.getContext('2d'),
+			
+			width = canvas.width = window.innerWidth,
+			height = canvas.height = window.innerHeight,
 
-      width = canvas.width = window.innerWidth,
-      height = canvas.height = window.innerHeight,
+			time = 4.9,
 
-      time = 4.9,
+			timeMax = height/6,
 
-      timeMax = height/6,
+			timeForward = true,
 
-      timeForward = true,
+			strokeW = 1,
 
-      strokeW = 1,
+			speed = .05,
 
-      speed = .05,
+			clearScreen = true,
+			pixles = [],
+			
+			pauseAnimation = true,
 
-      clearScreen = true,
-      pixles = [],
-      
-      pauseAnimation = true,
+			viewWidth = 1,
+			viewHeight = width/2;
 
-      viewWidth = 1,
-      viewHeight = width/2;
+	context.strokeStyle = 'white';
+	context.fillStyle = 'white';
 
-context.strokeStyle = 'white';
-context.fillStyle = 'white';
+	context.lineWidth = strokeW;
 
-context.lineWidth = strokeW;
+	canvas.style = `display: block;
+								position: static;
+								top: 0px;
+								left: 0px;
+								// cursor: none;
+								margin:auto;
+								background-color: black`;
 
-canvas.style = `display: block;
-                position: static;
-                top: 0px;
-                left: 0px;
-                // cursor: none;
-                margin:auto;
-                background-color: black`;
+	document.body.style = `margin: 0`;
 
-document.body.style = `margin: 0`;
+	document.body.appendChild(canvas)
 
-document.body.appendChild(canvas)
+	//USER INPUT EVENT LISTENER
+	document.addEventListener('keydown', userInputEvent, false);
 
-//USER INPUT EVENT LISTENER
-document.addEventListener('keydown', userInputEvent, false);
+	//USER INPUT LOGIC
+	function userInputEvent(input) {
 
-//USER INPUT LOGIC
-function userInputEvent(input) {
+		switch (input.code) {
 
-    switch (input.code) {
+				case 'Space':
+						pauseAnimation = !pauseAnimation;
+						if (!pauseAnimation) {
+								render()
+						}
+				break;
+				case 'ArrowUp':
+						speed = speed < 1 ? speed+.1 : 1;
+				break;
+				case "ArrowDown":
+						speed = speed > .2 ? speed-.1 : .2;
+		}
 
-        case 'Space':
-            pauseAnimation = !pauseAnimation;
-            if (!pauseAnimation) {
-                render()
-            }
-        break;
-        case 'ArrowUp':
-            speed = speed < 1 ? speed+.1 : 1;
-        break;
-        case "ArrowDown":
-            speed = speed > .2 ? speed-.1 : .2;
-    }
+		
+		
+	}
 
-    
-    
-}
+	//SET THE CANVAS ORIGIN TO THE MIDDLE OF THE WINDOW
+		context.translate(width/4, height/2)   
+		context.rotate(Math.PI/2)
 
-//SET THE CANVAS ORIGIN TO THE MIDDLE OF THE WINDOW
-    context.translate(width/4, height/2)   
-    context.rotate(Math.PI/2)
+	//ANIMAITON CYCLE
 
-//ANIMAITON CYCLE
+				render()
 
-        render()
+				function render() {
+				
+				while (time < 209) {
+						time+=.2
+						pixles = [];
+						calculate( 1)
+						calculate( 2)
+						renderPix()
+				}
+				time = 4.9;
+				seed1-=.007
+				seed2-=.007
 
-        function render() {
-        
-        while (time < 209) {
-            time+=.2
-            pixles = [];
-            calculate(-time, 1)
-            calculate(-time, 2)
-            renderPix()
-        }
-        time = 4.9;
-        seed1-=.005
-        seed2-=.005
+				if (!pauseAnimation) {
+						setTimeout(window.requestAnimationFrame, 0, render)
+				}
 
-
-        if (!pauseAnimation) {
-            setTimeout(window.requestAnimationFrame,  0, render)
-        }
-
-      }
+			}
 
 function renderPix() {
     pixles.forEach( p => {
@@ -113,10 +112,11 @@ function renderPix() {
     });
 }
 
-function calculate(s, num) {
+function calculate(num) {
     // transX = nRes == 77 ? s/30 : 0,
 
     const 
+    s = -time,
     nRes = time,
     seed = num === 1 ? seed1 : seed2;
     
@@ -128,19 +128,19 @@ function calculate(s, num) {
 
         for (let y = 0; y < viewHeight; y++) {
             const
-            offWidth = num === 2 ? width*2.7-s*2 : width*2.7+s*2,
-            offHeight =  num === 2 ? height*2.7-s*2 : height*2.7+s,
+            offWidth = num === 1 ? width*2.7-s*2 : width*2.7+s/5,
+            offHeight =  num === 1 ? height*2.7-s*2 : height*2.7+s,
             
             offX = (mapNumber(nRes, 12+(time*Math.abs(x/100))/1.4, timeMax, 0, offWidth)/nRes/4.2),
             offY = (mapNumber(nRes, (1+time/400)*(1+y*x/time*.1), timeMax, 0,offHeight)/nRes/1.2),
             noiseX = ((xCount*10)/(nRes*3))+offX+seed,
             noiseY = ((yCount*10)/(nRes*19.7))+offY+seed,
-            divid = time/1.3 < 54 ? time/1.3 : 54,
-            light = ((Math.abs((Noise(noiseX, noiseY)*divid )) % divid)+(nRes/5.33)+5)-y/24+10;
+            divid = time/1.4 < 63 ? time/1.4 : 63,
+            light = ((Math.abs((Noise(noiseX, noiseY)*divid )) % divid)+(nRes/5.5)+5)-y/23+15;
 
             if (light > 5) {
-                const color = (Math.abs((Noise(noiseX, noiseY) ))*Math.pow(divid,1.9))+s*1-127+y/1.3;
                 const 
+                color = (Math.abs((Noise(noiseX, noiseY) ))*Math.pow(divid,1.92))+s/3-127+y,
                 xPos = num === 1 ? x-time*3.5+20 : (-time*3.5+20)+(time*4-20)*3.5/2,
                 yPos = y-viewHeight/2;
                 
