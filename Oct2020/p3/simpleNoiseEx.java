ec647d916d3f1eba32612b5add7aba5c2bc9df47
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-13 22:24:37 -0400

    a more complex example of using openSimplexNoise with a Java animation. each pixel is calculated with noise

---
+++ Oct2020/p3/simpleNoiseEx.java
@@ -0,0 +1,27 @@
+
+OpenSimplexNoise noise;
+
+int width = 3840;
+int height = 2160;
+
+float xPos = random(1000);
+float yPos = random(1000);
+
+void setup() {
+  size(3840,2160);
+  noise = new OpenSimplexNoise();
+  
+}
+
+
+void draw() {
+  
+  background(0);
+  
+  xPos+=.01;
+  yPos+=.01;
+  
+  fill(254);
+  rect(width/2,height/2,100+noise(xPos)*100,100+noise(yPos)*100);
+ 
+}
