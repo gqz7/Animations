4e42b5ddb61d68f39e2967b6d05408cfa0b1c5c3
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-13 22:19:11 -0400

    animation updates with for video renders, star lines are shorter and more aboundant, black hole particles movie quicker

--- Oct2020/space-migration.js
+++ Oct2020/space-migration.js
@@ -1,4 +1,4 @@
-console.log(`Press Space to Start/Stop Animation,\n\n < and > (Comma and Period): cycle through diffrent 3D structures\n\nLeft/Right Arrow Keys: control how much of the object stays in view\n\nPress 'I' to display current settings\n\nPress 'M' to cycle through 4 color modes\n\nPress 'G' to toggle view in grayscale\n\nPress 'O' to decrease speed that complexity is being added to the object 'P' to increase speed\n\nPress 'L' to toggle camera locking on mouse position/auto-rotate\n\nPress 'F' to toggle flipping of structure's latitude and logitude coordinates`);
+console.log(`Press Space to Start/Stop Animation`);
 
 // alert('Look At Dev Console For Instructions\nFull Screen Recommended When You Click  \'OK\'')
 
@@ -61,38 +61,37 @@ let canvas = document.createElement('canvas');
 
     //set styling 
 
-    document.body.style = 'cursor: none; margin: 0px;';
-
     canvas.style = `display: block; position: static; top: 0px; left: 0px; cursor: none; margin:auto`
-
+    
+    document.body.style = 'cursor: none; margin: 0px;';
 
     document.body.style.backgroundColor = 'black';
 
     document.body.appendChild(canvas);
 
+    document.addEventListener('keydown', (e) => {
+        if (e.code == 'Space') {
+            renderPaused = !renderPaused;
+            if (!renderPaused) {
+                render()
+            }
+        }
+    })
+
     context.translate(width/2, height/2)
    
-create_star_field() //adds stars to Stars array
+    create_star_field() //adds stars to Stars array
 
    //ANIMATION CYCLE
      render()
 
       function render() {
-
-        // console.log(time);
         
-        time++ //a counter that counts the elapsed number of time
+        time+=.5 //a counter that counts the elapsed number of time
         clearFullScreen() //clear the canvas of previous animation cycle
-        
+    
         renderStars() //displays each star from its position in the Stars array 
-
-        moveStars(speed) //moves the position of each start slightly
-
         createSphere() //render the sphere
-
-        //counts how many time have occured
-        time++
-
 
         //user can toggle pausing of animation via 'spacebar'
         if (!renderPaused) {
@@ -103,7 +102,8 @@ create_star_field() //adds stars to Stars array
 
     function createSphere() {
 
-        let reso = time/cmplxSpd + 10,//resolution of sphere coord detail
+        let 
+            reso = 50,//resolution of sphere coord detail
             r = radius; //radius of sphere
     //first loop tracks longitude then the nested loop tracks latitude
         for (let i = 0; i < reso && i < 88; i++) {
@@ -112,48 +112,37 @@ create_star_field() //adds stars to Stars array
 
             for (let j = 0; j < reso; j++) {
 
-            let lat = mapNumber(j , 0, reso, 0, pi*2),
-                structureObj = {...(superSpos[SSindex])};
-
-            if (flipPos) {   
-                for (const key in structureObj) {
-                    structureObj[key] = !structureObj[key];
+                let lat = mapNumber(j , 0, reso, 0, pi*2),
+                    structureObj = {...(superSpos[SSindex])};
+                if (flipPos) {   
+                    for (const key in structureObj) {
+                        structureObj[key] = !structureObj[key];
+                    }
                 }
-            }
+                let x1 = structureObj.x1 ? lon : lat, x2 = structureObj.x2 ? lon : lat,
+                    y1 = structureObj.y1 ? lon : lat, y2 = structureObj.y2 ? lon : lat,
+                    z1 = structureObj.z  ? lon : lat;
 
-            let x1 = structureObj.x1 ? lon : lat, x2 = structureObj.x2 ? lon : lat,
-                y1 = structureObj.y1 ? lon : lat, y2 = structureObj.y2 ? lon : lat,
-                z1 = structureObj.z  ? lon : lat;
+                //formula for finding  xyz position based on polar angle in a xy system
+                const x = (r * Math.sin(x1) * Math.cos(x2)),
+                    y = (r * Math.cos(y1) * Math.sin(y2)),
+                    z =  r * Math.sin(z1);
 
-            //formula for finding  xyz position based on polar angle in a xy system
-            const x = (r * Math.sin(x1) * Math.cos(x2)),
-                  y = (r * Math.cos(y1) * Math.sin(y2)),
-                  z =  r * Math.sin(z1);
+                //store the points calculated into a object
+                coordinates = {
+                    x: x,
+                    y: y,
+                    z: z
+                };
 
-            //store the points calculated into a object
-            coordinates = {
-                x: x,
-                y: y,
-                z: z
-            };
-
-            let xRotation, yRotation;
-
-                xRotation = time/3333,
-                yRotation = time/3333;
-                rotateZ(time/7777)
-
-
-            //rotate the points about the origin to give the illusion of 3d
-            rotateX(xRotation)
-            rotateY(yRotation)
+                //rotate the points about the origin to give the illusion of 3d
+                rotateZ(time/777-1)
+                rotateX(time/777-3)
+                rotateY(time/777-5)
             
-            renderPoint(coordinates, j, i)
-
+                renderPoint(coordinates, j, i)
             }
-            
         }
-
     }
 
     //render an object's point's position onto the canvas
@@ -166,7 +155,7 @@ create_star_field() //adds stars to Stars array
             
             let size  = origin.z/107-.2>.88 ? origin.z/107 : .88;
 
-            context.fillStyle = `hsl(${dis*333+time*2}, 100%, ${light}%)`
+            context.fillStyle = `hsl(${dis*4444+time}, 100%, ${light}%)`
 
             const renderX = (origin.x/(dis/.5))//mapNumber(origin.x, 0, height/3, 0, dis)*130
             const renderY = (origin.y/(dis/.5))//mapNumber(origin.y, 0, height/3, 0, dis)*130
@@ -218,38 +207,7 @@ create_star_field() //adds stars to Stars array
     };
 
 
-
-   
-    //     setTimeout( () => {
-
-    //         if (time < 100) {
-
-    //             addStar()
-    //             addStar()
-    //             addStar()
-
-    //             // speed += .001
-                
-    //             render()
-                
-    //         } else if (time < 10000) {
-
-    //             // speed = speed <= 80 ? speed + .1 : 80;
-
-    //             render()
-                
-    //         } else {
-
-    //             return
-               
-    //         }
-
-    //     }, delay);
-    // }
-
-    // // FUNCTIONS
-
-    function create_star_streak(star, index) {
+    function create_star_streak(star) {
 
         let {x, y, v, lightness} = star;
 
@@ -260,82 +218,62 @@ create_star_field() //adds stars to Stars array
         let 
         x1 = x,
         y1 = y,
-        x2 = x*(1 + lightness/500),
-        y2 = y*(1 + lightness/500),
+        x2 = x*(1 + lightness/999),
+        y2 = y*(1 + lightness/999),
 
         grad = context.createLinearGradient(x1, y1, x2, y2);
 
         //set up gradient
-        grad.addColorStop(1, `hsl(0, 80%, ${lightness}%)`);
-        grad.addColorStop(6/7, `hsl(45, 80%, ${lightness}%)`);
-        grad.addColorStop(5/7, `hsl(90, 80%, ${lightness}%)`);
-        grad.addColorStop(4/7, `hsl(135, 80%, ${lightness}%)`);
-        grad.addColorStop(3/7, `hsl(180, 80%, ${lightness}%)`);
-        grad.addColorStop(2/7, `hsl(245, 80%, ${lightness}%)`);
-        grad.addColorStop(1/7, `hsl(305, 80%, ${lightness}%)`);
+        grad.addColorStop(1, `hsl(0, 100%, ${lightness}%)`);
+        grad.addColorStop(6/7, `hsl(45, 100%, ${lightness}%)`);
+        grad.addColorStop(5/7, `hsl(90, 100%, ${lightness}%)`);
+        grad.addColorStop(4/7, `hsl(135, 100%, ${lightness}%)`);
+        grad.addColorStop(3/7, `hsl(180, 100%, ${lightness}%)`);
+        grad.addColorStop(2/7, `hsl(245, 100%, ${lightness}%)`);
+        grad.addColorStop(1/7, `hsl(305, 100%, ${lightness}%)`);
         
         context.strokeStyle = grad;
-        
+        context.lineWidth = (lightness/100) + .2;
         //gradient line stroke 
         context.beginPath();
-        context.moveTo(x1,y1);
-        context.lineTo(x2,y2);
-
-        context.lineWidth = (lightness/100) + .2;
-       
+            context.moveTo(x1,y1);
+            context.lineTo(x2,y2);
         context.stroke();
-
-        // context.restore()
-
     }
 
-
     function create_star_field() {
-
-        for (let i = 0; i < 300; i++) {
-
+        for (let i = 0; i < 1000; i++) {
             let  
-                x =  (width * Math.random()) - width/2,
-                y =  (height * Math.random()) - height/2,
-
+                x = (width * Math.random()) - width/2,
+                y = (height * Math.random()) - height/2,
                 lightness = 10;
              
             Stars.push({
                 x: x, y: y, v: Math.random()/10, lightness: lightness
-            });
-            
-        }
-        
+            });   
+        }   
     }
 
-
-
     function renderStars() {
-
         for (let i = 0; i < Stars.length; i++) {
-            
-            create_star_streak(Stars[i], i);
-            
+            create_star_streak(Stars[i]);
         }
-
+        moveStars(speed) //moves the position of each start slightly
     }
 
     function moveStars(speed) {
 
         for (let i = 0; i < Stars.length; i++) {
 
-            const vel = Stars[i].v;
+            const velocity = Stars[i].v;
 
-            let NewX = Stars[i].x * (.979 + speed/4700 + Stars[i].lightness/1700+vel),
-                NewY = Stars[i].y * (.979 + speed/4700 + Stars[i].lightness/1700+vel);
-
+            let NewX = Stars[i].x * (.979 + speed/4700 + Stars[i].lightness/1700+velocity),
+                NewY = Stars[i].y * (.979 + speed/4700 + Stars[i].lightness/1700+velocity);
 
             if (NewX > width || NewX < -width || NewY > width/.5 || NewY < -width/.5) {
 
                 Stars.splice(i, 1); //if it goes off screen, delete it from the stars to be rendered
-
                 addStar() // then add a new one to replace it
-
                 i--
 
             } else {
@@ -343,7 +281,7 @@ create_star_field() //adds stars to Stars array
                 Stars[i].x = NewX;
                 Stars[i].y = NewY;
 
-                Stars[i].lightness = Stars[i].lightness <= 88 ? Stars[i].lightness * 1.02 : 88;
+                Stars[i].lightness = Stars[i].lightness <= 88 ? Stars[i].lightness * 1.01 : 88;
 
             }
            
@@ -355,7 +293,7 @@ create_star_field() //adds stars to Stars array
 
         let  
         ranAngle = Math.random() * 100   
-        radius = 7;
+        radius = 10;
         randomX1 = (Math.cos(ranAngle) * radius),
         randomY1 = (Math.sin(ranAngle) * radius),
         randomX2 = (Math.cos(ranAngle) * radius * 4),
@@ -363,16 +301,17 @@ create_star_field() //adds stars to Stars array
 
         ranNum = (Math.random() * 17) + 1,
 
-        x =  (randomX2 * ranNum) + (randomX1 * ranNum) * Math.random(),
-        y =  (randomY2 * ranNum) + (randomY1 * ranNum) * Math.random() * 5,
+        x = (randomX2 * ranNum) + (randomX1 * ranNum) * Math.random(),
+        y = (randomY2 * ranNum) + (randomY1 * ranNum) * Math.random() * 5,
 
         lightness = 10;
 
-        // console.log(radius);
-        
-
-        Stars.push({
-            x: x, y: y, lightness: lightness, v: Math.random()/10
-        });
-        
+        const starInCenter = Math.sqrt(Math.pow(x,2) + Math.pow(y,2)) < 200;
+        if (starInCenter) {
+            addStar()
+        } else {
+            Stars.push({
+                x: x, y: y, lightness: lightness, v: Math.random()/10
+            });
+        }
     }
